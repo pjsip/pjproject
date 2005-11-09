e85ddfeab7c0a25c79309afd6724c3f363604420
Benny Prijono <bennylp@teluu.com>
2005-11-09 16:45:27 +0000

    Organizing pjsip directory structure
    git-svn-id: https://svn.pjsip.org/repos/pjproject/main@42 74dad513-b988-da41-8d7b-12977e46ad98

---
+++ pjsip/include/pjsip_auth.h
@@ -0,0 +1,22 @@
+/* $Id$
+ *
+ */
+#ifndef __PJSIP_AUTH_H__
+#define __PJSIP_AUTH_H__
+
+/**
+ * @defgroup PJSIP_AUTH SIP Authorization module
+ */
+
+/**
+ * @file pjsip_auth.h
+ * @brief SIP Authorization Module.
+ */
+
+
+#include <pjsip_auth/sip_auth.h>
+#include <pjsip_auth/sip_auth_msg.h>
+#include <pjsip_auth/sip_auth_parser.h>
+
+#endif	/* __PJSIP_AUTH_H__ */
+
---
+++ pjsip/include/pjsip_simple.h
@@ -0,0 +1,26 @@
+/* $Id$
+ *
+ */
+
+/**
+ * @defgroup PJSIP_SIMPLE SIP Event, Instant Messaging and Presence Extension (SIMPLE)
+ */
+
+/**
+ * @file pjsip_simple.h
+ * @brief SIP SIMPLE Extension
+ */
+
+/*
+ * Include this header file to get all functionalities for SIMPLE extension
+ * (SIP for Instant Messaging and Presence Leveraging Extension).
+ */
+#ifndef __PJSIP_SIMPLE_H__
+#define __PJSIP_SIMPLE_H__
+
+#include <pjsip_simple/messaging.h>
+#include <pjsip_simple/event_notify.h>
+#include <pjsip_simple/pidf.h>
+#include <pjsip_simple/presence.h>
+
+#endif	/* __PJSIP_SIMPLE_H__ */
