e722461aefc60faf1e89b39a4ff444ed8657ac45
Benny Prijono <bennylp@teluu.com>
2005-11-13 19:40:44 +0000

    Applying license to pjproject
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@49 74dad513-b988-da41-8d7b-12977e46ad98

--- pjlib/src/pjlib-test/timer.c
+++ pjlib/src/pjlib-test/timer.c
@@ -1,5 +1,26 @@
 /* $Id$
  */
+/* 
+ * PJLIB - PJ Foundation Library
+ * (C)2003-2005 Benny Prijono <bennylp@bulukucing.org>
+ *
+ * Author:
+ *  Benny Prijono <bennylp@bulukucing.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 #include "test.h"
 
 /**
@@ -59,7 +80,7 @@ static int test_timer_heap(void)
     for (i=0; i<MAX_COUNT; ++i) {
 	entry[i].cb = &timer_callback;
     }
-    rc = pj_timer_heap_create(pool, MAX_COUNT, 0, &timer);
+    rc = pj_timer_heap_create(pool, MAX_COUNT, &timer);
     if (rc != PJ_SUCCESS) {
         app_perror("...error: unable to create timer heap", rc);
 	return -30;
