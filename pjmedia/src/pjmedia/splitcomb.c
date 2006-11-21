9aab47b1cd28ed348042f407ab2a8bc4649f2d6c
Benny Prijono <bennylp@teluu.com>
2006-11-21 22:55:16 +0000

    Increase splitcomb buffer count to 32
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@819 74dad513-b988-da41-8d7b-12977e46ad98

--- pjmedia/src/pjmedia/splitcomb.c
+++ pjmedia/src/pjmedia/splitcomb.c
@@ -27,7 +27,7 @@
 #define SIGNATURE_PORT	    PJMEDIA_PORT_SIGNATURE('S', 'p', 'C', 'P')
 #define THIS_FILE	    "splitcomb.c"
 #define TMP_SAMP_TYPE	    pj_int16_t
-#define MAX_BUF_CNT	    8
+#define MAX_BUF_CNT	    32
 
 #if 0
 #   define TRACE_UP_(x)	PJ_LOG(5,x)
