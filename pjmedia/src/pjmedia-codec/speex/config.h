df6be7292a00e1b8c7a13eb5ad71a16c3262a26a
Benny Prijono <bennylp@teluu.com>
2006-11-24 16:33:27 +0000

    Fixed speex logging to use PJLIB's log, because a GUI application doesn't have stderr
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@827 74dad513-b988-da41-8d7b-12977e46ad98

--- pjmedia/src/pjmedia-codec/speex/config.h
+++ pjmedia/src/pjmedia-codec/speex/config.h
@@ -1,5 +1,5 @@
 
-#include <pj/types.h>
+#include <pj/log.h>
 
 /* Check if we need to use the fixed point version */
 #if !defined(PJ_HAS_FLOATING_POINT) || PJ_HAS_FLOATING_POINT==0
@@ -19,3 +19,16 @@
 #   pragma warning(disable: 4305)   // truncation from 'const double ' to 'float '
 #   pragma warning(disable: 4701)   // local variable used without initialized
 #endif
+
+/*
+ * Override miscellaneous Speex functions.
+ */
+#define OVERRIDE_SPEEX_ERROR
+#define speex_error(str) PJ_LOG(4,("speex", "error: %s", str))
+
+#define OVERRIDE_SPEEX_WARNING
+#define speex_warning(str) PJ_LOG(5,("speex", "warning: %s", str))
+
+#define OVERRIDE_SPEEX_WARNING_INT
+#define speex_warning_int(str,val)  PJ_LOG(5,("speex", "warning: %s: %d", str, val))
+
