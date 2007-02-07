8148cda903420e5b2a436647c8ec87c856219571
Benny Prijono <bennylp@teluu.com>
2007-02-07 09:36:37 +0000

    Fixed ticket #92: undefined symbols when running Python application
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@935 74dad513-b988-da41-8d7b-12977e46ad98

--- pjsip-apps/src/py_pjsua/setup.py
+++ pjsip-apps/src/py_pjsua/setup.py
@@ -1,7 +1,28 @@
 from distutils.core import setup, Extension
 pjproject = "../../../"
+target = "i686-pc-linux-gnu"
 setup(name="py_pjsua", version="0.1",
 	ext_modules = [
-		Extension("py_pjsua", ["py_pjsua.c"], include_dirs=[pjproject + "pjsip/include", pjproject + "pjlib/include", pjproject + "pjlib-util/include", pjproject + "pjmedia/include"], library_dirs=[pjproject + "pjsip/lib", pjproject + "pjlib/lib", pjproject + "pjmedia/lib", pjproject + "pjlib-util/lib"], libraries=["pjsua-i686-pc-linux-gnu", "pjsip-ua-i686-pc-linux-gnu", "pjsip-simple-i686-pc-linux-gnu", "pjsip-i686-pc-linux-gnu", "pjmedia-codec-i686-pc-linux-gnu", "pjmedia-i686-pc-linux-gnu", "pjmedia-codec-i686-pc-linux-gnu", "pjlib-util-i686-pc-linux-gnu", "pj-i686-pc-linux-gnu"]),
-		
+		Extension("py_pjsua", 
+			  ["py_pjsua.c"], 
+			   include_dirs=[pjproject + "pjsip/include", 
+					 pjproject + "pjlib/include", 
+					 pjproject + "pjlib-util/include", 						 pjproject + "pjmedia/include"], 
+			   library_dirs=[pjproject + "pjsip/lib", 
+					 pjproject + "pjlib/lib", 
+					 pjproject + "pjmedia/lib", 
+					 pjproject + "pjlib-util/lib"], 
+			   libraries=[	"pjsua-" + target, 
+					"pjsip-ua-" + target, 
+					"pjsip-simple-" + target, 
+					"pjsip-" + target, 
+					"pjmedia-codec-" + target, 
+					"pjmedia-" + target, 
+					"pjmedia-codec-" + target, 
+					"pjlib-util-" + target, 
+					"pj-" + target,
+					"ssl",
+					"crypto",
+					"asound"]),
 	])
+
