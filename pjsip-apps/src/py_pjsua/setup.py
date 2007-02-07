63da4ff0d4ac4b514c8999078a7f02026c0ac93f
Benny Prijono <bennylp@teluu.com>
2007-02-07 10:09:19 +0000

    Fixed hardcoded Linux build in Python module's setup.py
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@936 74dad513-b988-da41-8d7b-12977e46ad98

--- pjsip-apps/src/py_pjsua/setup.py
+++ pjsip-apps/src/py_pjsua/setup.py
@@ -1,6 +1,23 @@
 from distutils.core import setup, Extension
+import os
+
 pjproject = "../../../"
-target = "i686-pc-linux-gnu"
+
+# Determine target
+#target = "i686-pc-linux-gnu"
+f = os.popen("grep TARGET_NAME ../../../build.mak")
+line = f.readline()
+tokens = line.split()
+found = 0
+for token in tokens:
+	if token == ":=" or token == "=":
+		found = 1
+	elif found != 0:
+		target = token
+		break
+
+print "Building py_pjsua module for " + target
+
 setup(name="py_pjsua", version="0.1",
 	ext_modules = [
 		Extension("py_pjsua", 
