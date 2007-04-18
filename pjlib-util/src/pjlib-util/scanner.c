23fd7ead9817c044e7974089fc93a20c184b7791
Benny Prijono <bennylp@teluu.com>
2007-04-18 15:40:44 +0000

    Fixed ticket #232: Parse failure on big endian machines (thanks Vladimir Hozjan)
    git-svn-id: https://svn.pjsip.org/repos/pjproject/branches/pjproject-0.5-stable@1203 74dad513-b988-da41-8d7b-12977e46ad98

--- pjlib-util/src/pjlib-util/scanner.c
+++ pjlib-util/src/pjlib-util/scanner.c
@@ -342,7 +342,9 @@ PJ_DEF(void) pj_scan_get_quote( pj_scanner *scanner,
 				int begin_quote, int end_quote, 
 				pj_str_t *out)
 {
-    pj_scan_get_quotes(scanner, (char*)&begin_quote, (char*)&end_quote, 1, out);
+    char beg = (char)begin_quote;
+    char end = (char)end_quote;
+    pj_scan_get_quotes(scanner, &beg, &end, 1, out);
 }
 
 PJ_DEF(void) pj_scan_get_quotes(pj_scanner *scanner,
