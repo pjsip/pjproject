ca1447ff25313b6da39c2b4d0d66bc2082d044f6
Benny Prijono <bennylp@teluu.com>
2007-04-28 04:13:11 +0000

    Fixed typo in PUBLISH documentation (thanks Klaus Darilion)
    git-svn-id: https://svn.pjsip.org/repos/pjproject/branches/split-3rd-party@1214 74dad513-b988-da41-8d7b-12977e46ad98

--- pjsip/include/pjsip-simple/publish.h
+++ pjsip/include/pjsip-simple/publish.h
@@ -32,13 +32,13 @@ PJ_BEGIN_DECL
 
 
 /**
- @defgroup PJSIP_SIMPLE_PUBLISH SIP Event State Publication (PUBLISH, RFC 3856)
+ @defgroup PJSIP_SIMPLE_PUBLISH SIP Event State Publication (PUBLISH, RFC 3903)
  @ingroup PJSIP_SIMPLE
- @brief Support for SIP Event State Publication (PUBLISH, RFC 3856)
+ @brief Support for SIP Event State Publication (PUBLISH, RFC 3903)
  @{
 
  This module contains the implementation of Session Initiation Protocol (SIP)
- Extension for Event State Publication (PUBLISH) as defined by RFC 3856.
+ Extension for Event State Publication (PUBLISH) as defined by RFC 3903.
  */
 
 /**
