a1852547d408dfad33e03ba0059fec36ecba345d
Benny Prijono <bennylp@teluu.com>
2007-05-30 07:28:33 +0000

    Another fix for ticket #290: Bug in WAV player when frame size is larger than file buffer size (thanks Samuel Vinson)
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@1321 74dad513-b988-da41-8d7b-12977e46ad98

--- pjmedia/src/pjmedia/wav_player.c
+++ pjmedia/src/pjmedia/wav_player.c
@@ -354,7 +354,7 @@ PJ_DEF(pj_status_t) pjmedia_wav_player_port_create( pj_pool_t *pool,
     /* samples_per_frame must be smaller than bufsize (because get_frame()
      * doesn't handle this case).
      */
-    if (fport->base.info.samples_per_frame*fport->base.info.bits_per_sample >=
+    if (fport->base.info.samples_per_frame * BYTES_PER_SAMPLE >=
 	fport->bufsize)
     {
 	pj_file_close(fport->fd);
