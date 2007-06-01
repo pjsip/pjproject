e0f7104ac477ac8e1573862f1ac21a312df747f8
Benny Prijono <bennylp@teluu.com>
2007-06-01 00:06:08 +0000

    Added assert for worker thread count in pjmedia endpoint (thanks Phil Torre)
    git-svn-id: https://svn.pjsip.org/repos/pjproject/branches/pjproject-0.5-stable@1331 74dad513-b988-da41-8d7b-12977e46ad98

--- pjmedia/src/pjmedia/endpoint.c
+++ pjmedia/src/pjmedia/endpoint.c
@@ -121,6 +121,7 @@ PJ_DEF(pj_status_t) pjmedia_endpt_create(pj_pool_factory *pf,
     }
 
     PJ_ASSERT_RETURN(pf && p_endpt, PJ_EINVAL);
+    PJ_ASSERT_RETURN(worker_cnt <= MAX_THREADS, PJ_EINVAL);
 
     pool = pj_pool_create(pf, "med-ept", 512, 512, NULL);
     if (!pool)
