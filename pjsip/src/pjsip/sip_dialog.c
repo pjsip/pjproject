76c18818c9fe8131f86973f4f8179181e79ceefb
Benny Prijono <bennylp@teluu.com>
2007-09-30 11:00:49 +0000

    Changed initial sequence number randomization to produce number in 15bit instead, to assist troubleshooting
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@1459 74dad513-b988-da41-8d7b-12977e46ad98

--- pjsip/src/pjsip/sip_dialog.c
+++ pjsip/src/pjsip/sip_dialog.c
@@ -199,7 +199,7 @@ PJ_DEF(pj_status_t) pjsip_dlg_create_uac( pjsip_user_agent *ua,
 				       dlg->local.info->tag.slen);
 
     /* Randomize local CSeq. */
-    dlg->local.first_cseq = pj_rand() & 0x7FFFFFFFL;
+    dlg->local.first_cseq = pj_rand() & 0x7FFF;
     dlg->local.cseq = dlg->local.first_cseq;
 
     /* Init local contact. */
@@ -367,7 +367,7 @@ PJ_DEF(pj_status_t) pjsip_dlg_create_uas(   pjsip_user_agent *ua,
 
 
     /* Randomize local cseq */
-    dlg->local.first_cseq = pj_rand() & 0x7FFFFFFFL;
+    dlg->local.first_cseq = pj_rand() & 0x7FFF;
     dlg->local.cseq = dlg->local.first_cseq;
 
     /* Init local contact. */
