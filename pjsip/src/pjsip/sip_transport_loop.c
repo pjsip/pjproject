423f64146589c35110e806c402ca538f415e4c71
Benny Prijono <bennylp@teluu.com>
2008-01-26 10:45:02 +0000

    Bug: source address not initialized in loop transport causing pjsip-test to fail
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@1756 74dad513-b988-da41-8d7b-12977e46ad98

--- pjsip/src/pjsip/sip_transport_loop.c
+++ pjsip/src/pjsip/sip_transport_loop.c
@@ -89,12 +89,16 @@ struct recv_list *create_incoming_packet( struct loop_transport *loop,
 	      tdata->buf.cur - tdata->buf.start);
     pkt->rdata.pkt_info.len = tdata->buf.cur - tdata->buf.start;
 
+    /* the source address */
+    pkt->rdata.pkt_info.src_addr.addr.sa_family = pj_AF_INET();
+
     /* "Source address" info. */
     pkt->rdata.pkt_info.src_addr_len = sizeof(pj_sockaddr_in);
-    if (loop->base.key.type == PJSIP_TRANSPORT_LOOP)
+    if (loop->base.key.type == PJSIP_TRANSPORT_LOOP) {
 	pj_ansi_strcpy(pkt->rdata.pkt_info.src_name, ADDR_LOOP);
-    else
+    } else {
 	pj_ansi_strcpy(pkt->rdata.pkt_info.src_name, ADDR_LOOP_DGRAM);
+    }
     pkt->rdata.pkt_info.src_port = loop->base.local_name.port;
 
     /* When do we need to "deliver" this packet. */
