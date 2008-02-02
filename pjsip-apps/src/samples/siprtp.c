1a68b66db218933fc9741e7980cc38210e1ac133
Benny Prijono <bennylp@teluu.com>
2008-02-02 09:09:27 +0000

    Fixed warning about uninitialized tpinfo variable
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@1768 74dad513-b988-da41-8d7b-12977e46ad98

--- pjsip-apps/src/samples/siprtp.c
+++ pjsip-apps/src/samples/siprtp.c
@@ -1012,6 +1012,7 @@ static pj_status_t create_sdp( pj_pool_t *pool,
 
 
     /* Get transport info */
+    pj_bzero(&tpinfo, sizeof(tpinfo));
     pjmedia_transport_get_info(audio->transport, &tpinfo);
 
     /* Create and initialize basic SDP session */
