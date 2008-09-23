d61e5327831c25c8a5c43fca232cd1909a73b7aa
Benny Prijono <bennylp@teluu.com>
2008-09-23 09:11:20 +0000

    More ticket #639: fixed warning with gcc about comparison always true
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@2307 74dad513-b988-da41-8d7b-12977e46ad98

--- pjlib/include/pj/ctype.h
+++ pjlib/include/pj/ctype.h
@@ -62,7 +62,7 @@ PJ_INLINE(int) pj_isalpha(unsigned char c) { return isalpha(c); }
  * @return      Non-zero value if c is a particular representation of 
  *              an ASCII character.
  */
-PJ_INLINE(int) pj_isascii(unsigned char c) { return c<128 && c>=0; }
+PJ_INLINE(int) pj_isascii(unsigned char c) { return c<128; }
 
 /** 
  * Returns a non-zero value if c is a particular representation of 
