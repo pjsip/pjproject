9be224f1e8c55d9a3a38684a7bcc57d89691a72f
Benny Prijono <bennylp@teluu.com>
2008-12-29 17:57:13 +0000

    Added copyright text in python test files
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@2401 74dad513-b988-da41-8d7b-12977e46ad98

--- tests/cdash/cfg_gnu.py
+++ tests/cdash/cfg_gnu.py
@@ -1,3 +1,22 @@
+#
+# cfg_gnu.py - GNU target configurator
+#
+# Copyright (C) 2008-2009 Teluu Inc. (http://www.teluu.com)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
 import builder
 import os
 import sys
@@ -13,8 +32,8 @@ def create_builder(args):
         file = "cfg_site"
 
     if os.access(file+".py", os.F_OK) == False:
-	print "Error: file '%s.py' doesn't exist." % (file)
-	sys.exit(1)
+        print "Error: file '%s.py' doesn't exist." % (file)
+        sys.exit(1)
 
     cfg_site = __import__(file)
     test_cfg = builder.BaseConfig(cfg_site.BASE_DIR, \
