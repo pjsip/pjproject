3978603f6a7d5847986cee80370508ce6d08d673
Benny Prijono <bennylp@teluu.com>
2008-12-29 15:04:00 +0000

    Changed eol-style on README.TXT, and clean up cfg_site_sample.py
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@2400 74dad513-b988-da41-8d7b-12977e46ad98

--- tests/cdash/cfg_site_sample.py
+++ tests/cdash/cfg_site_sample.py
@@ -1,7 +1,7 @@
 import builder
 
 # Your site name
-SITE_NAME="Newham2"
+SITE_NAME="Newham3"
 
 # The URL where tests will be submitted to
 URL = "http://192.168.0.2/dash/submit.php?project=PJSIP"
@@ -14,12 +14,12 @@ BASE_DIR = "/root/project/pjproject"
 
 # List of additional ccdash options
 #OPTIONS = ["-o", "out.xml", "-y"]
-OPTIONS = ["-o", "out.xml"]
+OPTIONS = []
 
 # List of regular expression of test patterns to be excluded
-EXCLUDE = [".*"]
+EXCLUDE = []
 
 # List of regular expression of test patterns to be included (even
 # if they match EXCLUDE patterns)
-NOT_EXCLUDE = ["run.py mod_run.*100_simple"]
+NOT_EXCLUDE = []
 #"configure", "update", "build.*make", "build", "run.py mod_run.*100_simple"]
