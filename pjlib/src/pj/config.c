fbb6810c9ac82d56e6ca8c549f3f91f1bb49ba13
Benny Prijono <bennylp@teluu.com>
2009-03-19 19:56:28 +0000

    Updated version to 1.1-trunk
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@2526 74dad513-b988-da41-8d7b-12977e46ad98

--- pjlib/src/pj/config.c
+++ pjlib/src/pj/config.c
@@ -22,7 +22,7 @@
 #include <pj/ioqueue.h>
 
 static const char *id = "config.c";
-PJ_DEF_DATA(const char*) PJ_VERSION = "1.1";
+PJ_DEF_DATA(const char*) PJ_VERSION = "1.1-trunk";
 
 /*
  * Get PJLIB version string.
