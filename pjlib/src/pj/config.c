a1fd6fff7deef700dbbb7b0f9e3c4a915bfd142f
Benny Prijono <bennylp@teluu.com>
2009-07-02 14:50:38 +0000

    Changed version to 1.0.3
    git-svn-id: https://svn.pjsip.org/repos/pjproject/branches/1.0@2828 74dad513-b988-da41-8d7b-12977e46ad98

--- pjlib/src/pj/config.c
+++ pjlib/src/pj/config.c
@@ -22,7 +22,7 @@
 #include <pj/ioqueue.h>
 
 static const char *id = "config.c";
-PJ_DEF_DATA(const char*) PJ_VERSION = "1.0.2-trunk";
+PJ_DEF_DATA(const char*) PJ_VERSION = "1.0.3";
 
 /*
  * Get PJLIB version string.
