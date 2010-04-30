d4933d7454ea6a595aefca407b3317b5ae436fd9
Benny Prijono <bennylp@teluu.com>
2010-04-30 14:11:58 +0000

    Changed version to 1.6-trunk
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@3157 74dad513-b988-da41-8d7b-12977e46ad98

--- pjlib/src/pj/config.c
+++ pjlib/src/pj/config.c
@@ -22,7 +22,7 @@
 #include <pj/ioqueue.h>
 
 static const char *id = "config.c";
-PJ_DEF_DATA(const char*) PJ_VERSION = "1.6";
+PJ_DEF_DATA(const char*) PJ_VERSION = "1.6-trunk";
 
 /*
  * Get PJLIB version string.
--- version.mak
+++ version.mak
@@ -1,1 +1,1 @@
-export PJ_VERSION := 1.6
+export PJ_VERSION := 1.6-trunk
