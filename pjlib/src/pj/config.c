6640ad1fe56d7a5f4a7a05503524f4efc1338f41
Benny Prijono <bennylp@teluu.com>
2010-10-21 03:03:42 +0000

    Changed version to 1.8.5
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@3353 74dad513-b988-da41-8d7b-12977e46ad98

--- pjlib/src/pj/config.c
+++ pjlib/src/pj/config.c
@@ -22,7 +22,7 @@
 #include <pj/ioqueue.h>
 
 static const char *id = "config.c";
-PJ_DEF_DATA(const char*) PJ_VERSION = "1.8-trunk";
+PJ_DEF_DATA(const char*) PJ_VERSION = "1.8.5";
 
 /*
  * Get PJLIB version string.
--- version.mak
+++ version.mak
@@ -1,1 +1,1 @@
-export PJ_VERSION := 1.8-trunk
+export PJ_VERSION := 1.8.5
