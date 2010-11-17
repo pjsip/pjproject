ae51ee052899c402d4f7169f076b27f116acb9a2
Benny Prijono <bennylp@teluu.com>
2010-11-17 09:00:17 +0000

    Fixed #1158: Possible wrong multiplexing of STUN traffic inside TURN Data Indication
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@3369 74dad513-b988-da41-8d7b-12977e46ad98

--- pjnath/src/pjnath/ice_session.c
+++ pjnath/src/pjnath/ice_session.c
@@ -2894,8 +2894,13 @@ PJ_DEF(pj_status_t) pj_ice_sess_on_rx_pkt(pj_ice_sess *ice,
 	return PJ_EINVAL;
     }
 
+    /* Don't check fingerprint. We only need to distinguish STUN and non-STUN
+     * packets. We don't need to verify the STUN packet too rigorously, that
+     * will be done by the user.
+     */
     status = pj_stun_msg_check((const pj_uint8_t*)pkt, pkt_size, 
-    			       PJ_STUN_IS_DATAGRAM);
+    			       PJ_STUN_IS_DATAGRAM |
+    			         PJ_STUN_NO_FINGERPRINT_CHECK);
     if (status == PJ_SUCCESS) {
 	status = pj_stun_session_on_rx_pkt(comp->stun_sess, pkt, pkt_size,
 					   PJ_STUN_IS_DATAGRAM, msg_data,
