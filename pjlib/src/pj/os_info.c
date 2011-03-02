c808952e7c7b1c4d6bc3196801b992edf2c8eeee
Sauw Ming <ming@teluu.com>
2011-03-02 10:53:24 +0000

    Re #1202: Fixed compilation preprocessor for os_info so that it's more robust across various device and simulator SDKs.
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@3436 74dad513-b988-da41-8d7b-12977e46ad98

--- pjlib/src/pj/os_info.c
+++ pjlib/src/pj/os_info.c
@@ -139,7 +139,7 @@ PJ_DEF(const pj_sys_info*) pj_get_sys_info(void)
      */
 #if defined(PJ_HAS_UNAME) && PJ_HAS_UNAME
     #if defined(PJ_DARWINOS) && PJ_DARWINOS != 0 && TARGET_OS_IPHONE && \
-	TARGET_IPHONE_SIMULATOR == 0
+	(!defined TARGET_IPHONE_SIMULATOR || TARGET_IPHONE_SIMULATOR == 0)
     {
 	pj_str_t buf = {si_buffer + PJ_SYS_INFO_BUFFER_SIZE - left, left};
 	pj_str_t machine = {"arm", 3};
--- pjlib/src/pj/os_info_iphone.m
+++ pjlib/src/pj/os_info_iphone.m
@@ -17,7 +17,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
  */
-#ifdef TARGET_IPHONE_SIMULATOR == 0
+#include "TargetConditionals.h"
+
+#if !defined TARGET_IPHONE_SIMULATOR || TARGET_IPHONE_SIMULATOR == 0
 
 #include <pj/os.h>
 #include <pj/string.h>
