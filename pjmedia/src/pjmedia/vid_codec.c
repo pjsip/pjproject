921e8130b855983294e58a5aeae811a334ba2c1a
Benny Prijono <bennylp@teluu.com>
2011-07-07 07:37:55 +0000

    Misc (re #1320): minor, changed prio parameter in pjmedia_vid_codec_mgr_enum_codecs() from pointer to array to clarify its use
    git-svn-id: https://svn.pjsip.org/repos/pjproject/branches/projects/2.0-dev@3607 74dad513-b988-da41-8d7b-12977e46ad98

--- pjmedia/src/pjmedia/vid_codec.c
+++ pjmedia/src/pjmedia/vid_codec.c
@@ -274,7 +274,7 @@ PJ_DEF(pj_status_t) pjmedia_vid_codec_mgr_enum_codecs(
                                 pjmedia_vid_codec_mgr *mgr, 
 			        unsigned *count, 
 			        pjmedia_vid_codec_info codecs[],
-			        unsigned *prio)
+			        unsigned prio[])
 {
     unsigned i;
 
