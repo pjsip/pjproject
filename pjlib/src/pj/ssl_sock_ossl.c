e4875da904171647be17a6ab9d73cc90193dab5a
Nanang Izzuddin <nanang@teluu.com>
2011-07-07 08:39:53 +0000

    Fixed #1321: Put OPENSSL_NO_SSL2 macro check for all usages of OpenSSL API SSLv2*().
    git-svn-id: https://svn.pjsip.org/repos/pjproject/branches/1.x@3610 74dad513-b988-da41-8d7b-12977e46ad98

--- pjlib/src/pj/ssl_sock_ossl.c
+++ pjlib/src/pj/ssl_sock_ossl.c
@@ -310,8 +310,10 @@ static pj_status_t init_openssl(void)
 	    meth = (SSL_METHOD*)TLSv1_server_method();
 	if (!meth)
 	    meth = (SSL_METHOD*)SSLv3_server_method();
+#ifndef OPENSSL_NO_SSL2
 	if (!meth)
 	    meth = (SSL_METHOD*)SSLv2_server_method();
+#endif
 	pj_assert(meth);
 
 	ctx=SSL_CTX_new(meth);
@@ -488,9 +490,11 @@ static pj_status_t create_ssl(pj_ssl_sock_t *ssock)
     case PJ_SSL_SOCK_PROTO_TLS1:
 	ssl_method = (SSL_METHOD*)TLSv1_method();
 	break;
+#ifndef OPENSSL_NO_SSL2
     case PJ_SSL_SOCK_PROTO_SSL2:
 	ssl_method = (SSL_METHOD*)SSLv2_method();
 	break;
+#endif
     case PJ_SSL_SOCK_PROTO_SSL3:
 	ssl_method = (SSL_METHOD*)SSLv3_method();
 	break;
