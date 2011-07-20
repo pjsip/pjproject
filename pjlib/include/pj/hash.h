521b9f2fc0fada81f577fe2309b337e1baeee694
Benny Prijono <bennylp@teluu.com>
2011-07-20 08:08:46 +0000

    Misc (re #1252): wrong doxygen documentation on the key argument of pj_hash_set, which said that it has to be valid throughout the duration of the entry. This is no longer true. Thanks Johan Lantz for pointing this out.
    git-svn-id: https://svn.pjsip.org/repos/pjproject/branches/1.x@3673 74dad513-b988-da41-8d7b-12977e46ad98

--- pjlib/include/pj/hash.h
+++ pjlib/include/pj/hash.h
@@ -125,8 +125,8 @@ PJ_DECL(void *) pj_hash_get( pj_hash_table_t *ht,
  * @param pool	    the pool to allocate the new entry if a new entry has to be
  *		    created.
  * @param ht	    the hash table.
- * @param key	    the key, which MUST point to buffer that remains valid
- *		    for the duration of the entry.
+ * @param key	    the key. If pool is not specified, the key MUST point to
+ * 		    buffer that remains valid for the duration of the entry.
  * @param keylen    the length of the key, or PJ_HASH_KEY_STRING to use the 
  *		    string length of the key.
  * @param hval	    if the value is not zero, then the hash table will use
