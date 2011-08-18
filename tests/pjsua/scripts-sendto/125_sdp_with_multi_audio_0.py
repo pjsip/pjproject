aa967ad7da77b69c10980b92c08313e03a4c164a
Nanang Izzuddin <nanang@teluu.com>
2011-08-18 17:31:46 +0000

    Re #1346 (python test fixes): adjust test scenario to follow the new behavior of pjsua in selecting active media from multiple media with same types, i.e: it will select the first media (previously/1.x it will will select the last).
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@3711 74dad513-b988-da41-8d7b-12977e46ad98

--- tests/pjsua/scripts-sendto/125_sdp_with_multi_audio_0.py
+++ tests/pjsua/scripts-sendto/125_sdp_with_multi_audio_0.py
@@ -3,8 +3,7 @@ import inc_sip as sip
 import inc_sdp as sdp
 
 # Multiple good m=audio lines! The current algorithm in pjsua-lib will
-# select the last audio (which should be okay, as we're entitled to
-# select any of them)
+# select the first audio (note that in 1.x it will select the last audio)
 sdp = \
 """
 v=0
@@ -20,7 +19,7 @@ m=audio 3000 RTP/AVP 0
 pjsua_args = "--null-audio --auto-answer 200"
 extra_headers = ""
 include = ["Content-Type: application/sdp",	# response must include SDP
-	   "m=audio 0 RTP/AVP[\\s\\S]+m=audio 0 RTP/AVP[\\s\\S]+m=audio [1-9]+[0-9]* RTP/AVP"
+	   "m=audio [1-9]+[0-9]* RTP/AVP[\\s\\S]+m=audio 0 RTP/AVP[\\s\\S]+m=audio 0 RTP/AVP"
 	   ]
 exclude = []
 
