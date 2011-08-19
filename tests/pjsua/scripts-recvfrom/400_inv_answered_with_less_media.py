bb6f57facacdbf36d33fb365f25754e664a6c505
Nanang Izzuddin <nanang@teluu.com>
2011-08-19 12:24:48 +0000

    Re #1346 (python test fixes): Updated test scenario that requires pjsua to generate multiple media offer using --extra-audio instead of using --use-srtp=3 (optional-with-duplicated-media).
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@3716 74dad513-b988-da41-8d7b-12977e46ad98

--- tests/pjsua/scripts-recvfrom/400_inv_answered_with_less_media.py
+++ tests/pjsua/scripts-recvfrom/400_inv_answered_with_less_media.py
@@ -4,7 +4,7 @@ import inc_sdp as sdp
 
 # Offer with 2 media lines answered with only 1 media line
 
-pjsua = "--null-audio sip:127.0.0.1:$PORT --use-srtp=3 --srtp-secure=0"
+pjsua = "--null-audio sip:127.0.0.1:$PORT --id=sip:1000@localhost --extra-audio --use-srtp=0"
 
 sdp = \
 """
@@ -21,7 +21,7 @@ a=fmtp:101 0-15
 """
 
 req = sip.RecvfromTransaction("Receiving 2 media lines, answer with 1 media line", 200,
-				include=["m=audio \d+ RTP/SAVP", "m=audio \d+ RTP/AVP"], 
+				include=["m=audio \d+ RTP/AVP", "m=audio \d+ RTP/AVP"], 
 				exclude=[],
                                 resp_hdr=["Content-type: application/sdp"],
                                 resp_body=sdp,
