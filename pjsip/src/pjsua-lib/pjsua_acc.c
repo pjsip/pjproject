f1e64e5d43728386a0eb908f6be95bfc1d5d9dda
Benny Prijono <bennylp@teluu.com>
2012-04-10 05:46:04 +0000

    Fixed #1481: Immediately destroy the registration instance when TCP/TLS transport gets disconnected
    git-svn-id: https://svn.pjsip.org/repos/pjproject/branches/1.x@4033 74dad513-b988-da41-8d7b-12977e46ad98

--- pjsip/src/pjsua-lib/pjsua_acc.c
+++ pjsip/src/pjsua-lib/pjsua_acc.c
@@ -2821,6 +2821,12 @@ void pjsua_acc_on_tp_state_changed(pjsip_transport *tp,
 	    continue;
 	}
 
+	/* Destroy regc to release transport immediately */
+	if (pjsua_var.acc[i].regc) {
+	    pjsip_regc_destroy(pjsua_var.acc[i].regc);
+	}
+	pjsua_var.acc[i].regc = NULL;
+
 	/* Schedule reregistration for this account */
 	schedule_reregistration(acc);
     }
