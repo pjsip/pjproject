7e1a8756fe506142c99cf29efaaee352e569f6d7
Nanang Izzuddin <nanang@teluu.com>
2012-05-22 11:30:26 +0000

    Changed version to 1.14.2
    git-svn-id: https://svn.pjsip.org/repos/pjproject/branches/1.x@4142 74dad513-b988-da41-8d7b-12977e46ad98

--- pjlib/include/pj/config.h
+++ pjlib/include/pj/config.h
@@ -1125,13 +1125,13 @@ PJ_BEGIN_DECL
 #define PJ_VERSION_NUM_MINOR	14
 
 /** PJLIB version revision number. */
-#define PJ_VERSION_NUM_REV	0
+#define PJ_VERSION_NUM_REV	2
 
 /**
  * Extra suffix for the version (e.g. "-svn"), or empty for
  * web release version.
  */
-#define PJ_VERSION_NUM_EXTRA	"-svn"
+#define PJ_VERSION_NUM_EXTRA	""
 
 /**
  * PJLIB version number consists of three bytes with the following format:
