8a208a2e0a8f1776d0eb3a6114a8bd61877113ac
Sauw Ming <ming@teluu.com>
2012-07-19 01:59:38 +0000

    Re #1516: Add sample config for Android build Please refer to [http://trac.pjsip.org/repos/wiki/Getting-Started/Android Android Getting Started guide]
    git-svn-id: https://svn.pjsip.org/repos/pjproject/branches/projects/android@4211 74dad513-b988-da41-8d7b-12977e46ad98

--- pjlib/include/pj/config_site_sample.h
+++ pjlib/include/pj/config_site_sample.h
@@ -359,6 +359,70 @@
 #endif
 
 /*
+ * Android sample settings.
+ */
+#if PJ_CONFIG_ANDROID
+
+    #define PJ_ANDROID                          1
+
+    /*
+     * PJLIB settings.
+     */
+
+    /* Disable floating point support */
+    #undef PJ_HAS_FLOATING_POINT
+    #define PJ_HAS_FLOATING_POINT		0
+
+    /*
+     * PJMEDIA settings
+     */
+
+    /* We have our own OpenSL ES backend */
+    #define PJMEDIA_AUDIO_DEV_HAS_PORTAUDIO	0
+    #define PJMEDIA_AUDIO_DEV_HAS_WMME		0
+    #define PJMEDIA_AUDIO_DEV_HAS_OPENSL        1
+
+    /* Disable some codecs */
+    #define PJMEDIA_HAS_L16_CODEC		0
+    #define PJMEDIA_HAS_G722_CODEC		0
+
+    /* Fine tune Speex's default settings for best performance/quality */
+    #define PJMEDIA_CODEC_SPEEX_DEFAULT_QUALITY	5
+
+    /*
+     * PJSIP settings.
+     */
+
+    /* Increase allowable packet size, just in case */
+    //#define PJSIP_MAX_PKT_LEN			2000
+
+    /*
+     * PJSUA settings.
+     */
+
+    /* Default codec quality, previously was set to 5, however it is now
+     * set to 4 to make sure pjsua instantiates resampler with small filter.
+     */
+    #define PJSUA_DEFAULT_CODEC_QUALITY		4
+
+    /* Set maximum number of dialog/transaction/calls to minimum */
+    #define PJSIP_MAX_TSX_COUNT 		31
+    #define PJSIP_MAX_DIALOG_COUNT 		31
+    #define PJSUA_MAX_CALLS			4
+
+    /* Other pjsua settings */
+    #define PJSUA_MAX_ACC			4
+    #define PJSUA_MAX_PLAYERS			4
+    #define PJSUA_MAX_RECORDERS			4
+    #define PJSUA_MAX_CONF_PORTS		(PJSUA_MAX_CALLS+2*PJSUA_MAX_PLAYERS)
+    #define PJSUA_MAX_BUDDIES			32
+
+    /* pjsua_app settings */
+    #define USE_GUI                             1
+#endif
+
+
+/*
  * Minimum size
  */
 #ifdef PJ_CONFIG_MINIMAL_SIZE
