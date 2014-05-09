da23a224b68877e5df003d6d60fd9dc2157e0238
Benny Prijono <bennylp@teluu.com>
2014-05-09 08:50:08 +0000

    Re #1768: fixed wrong JNI compilation flags used by pjsua2 Android sample app
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@4841 74dad513-b988-da41-8d7b-12977e46ad98

--- pjsip-apps/src/swig/java/android/jni/Android.mk
+++ pjsip-apps/src/swig/java/android/jni/Android.mk
@@ -4,9 +4,10 @@ LOCAL_PATH	:= $(PJDIR)/pjsip-apps/src/swig/java/android
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := libpjsua2
-LOCAL_CFLAGS    := $(APP_CFLAGS) -frtti -fexceptions
-LOCAL_LDFLAGS   := $(APP_LDFLAGS)
-LOCAL_LDLIBS    := $(APP_LDLIBS)
+LOCAL_CFLAGS    := $(APP_CXXFLAGS) -frtti -fexceptions
+LOCAL_LDFLAGS   := $(APP_LDXXFLAGS)
+LOCAL_LDLIBS    := $(APP_LDXXLIBS)
+#LOCAL_SHARED_LIBRARIES := $(APP_LDXXLIBS)
 LOCAL_SRC_FILES := ../output/pjsua2_wrap.cpp
 
 include $(BUILD_SHARED_LIBRARY)
