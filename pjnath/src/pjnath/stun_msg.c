30b3ed7079d0fa9c53c083a8f05347ab8ae850af
Benny Prijono <bennylp@teluu.com>
2014-08-20 10:35:29 +0000

    Fixed #1780: Bug: STUN string attribute has zero length
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@4895 74dad513-b988-da41-8d7b-12977e46ad98

--- pjnath/src/pjnath/stun_msg.c
+++ pjnath/src/pjnath/stun_msg.c
@@ -1137,10 +1137,12 @@ PJ_DEF(pj_status_t) pj_stun_string_attr_init( pj_stun_string_attr *attr,
 					      int attr_type,
 					      const pj_str_t *value)
 {
-    INIT_ATTR(attr, attr_type, 0);
     if (value && value->slen) {
+	INIT_ATTR(attr, attr_type, value->slen);
 	attr->value.slen = value->slen;
 	pj_strdup(pool, &attr->value, value);
+    } else {
+	INIT_ATTR(attr, attr_type, 0);
     }
     return PJ_SUCCESS;
 }
