664b8debd2a8f10ee9ecfeaab53d6a5e475ad46b
Nanang Izzuddin <nanang@teluu.com>
2014-08-26 11:01:57 +0000

    Re #1757: Set OpenGL as default renderer on iOS (initialized before UIView renderer).
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@4908 74dad513-b988-da41-8d7b-12977e46ad98

--- pjmedia/src/pjmedia-videodev/videodev.c
+++ pjmedia/src/pjmedia-videodev/videodev.c
@@ -381,11 +381,11 @@ PJ_DEF(pj_status_t) pjmedia_vid_dev_subsys_init(pj_pool_factory *pf)
 #if PJMEDIA_VIDEO_DEV_HAS_QT
     vid_subsys.drv[vid_subsys.drv_cnt++].create = &pjmedia_qt_factory;
 #endif
-#if PJMEDIA_VIDEO_DEV_HAS_IOS
-    vid_subsys.drv[vid_subsys.drv_cnt++].create = &pjmedia_ios_factory;
-#endif
 #if PJMEDIA_VIDEO_DEV_HAS_OPENGL
     vid_subsys.drv[vid_subsys.drv_cnt++].create = &pjmedia_opengl_factory;
+#endif
+#if PJMEDIA_VIDEO_DEV_HAS_IOS
+    vid_subsys.drv[vid_subsys.drv_cnt++].create = &pjmedia_ios_factory;
 #endif
 #if PJMEDIA_VIDEO_DEV_HAS_DSHOW
     vid_subsys.drv[vid_subsys.drv_cnt++].create = &pjmedia_dshow_factory;
