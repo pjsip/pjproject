65219d45f0f4a22deef5e7ec611c94829c586715
Nanang Izzuddin <nanang@teluu.com>
2014-09-02 03:21:38 +0000

    Fix #1781: Don't update dialog route set from UPDATE response.
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@4911 74dad513-b988-da41-8d7b-12977e46ad98

--- pjsip/src/pjsip/sip_dialog.c
+++ pjsip/src/pjsip/sip_dialog.c
@@ -1689,12 +1689,17 @@ static void dlg_update_routeset(pjsip_dialog *dlg, const pjsip_rx_data *rdata)
     const pjsip_hdr *hdr, *end_hdr;
     //pj_int32_t msg_cseq;
     const pjsip_msg *msg;
+    const pjsip_method update = { PJSIP_OTHER_METHOD, {"UPDATE", 6}};
 
     msg = rdata->msg_info.msg;
     //msg_cseq = rdata->msg_info.cseq->cseq;
 
     /* Ignore if route set has been frozen */
     if (dlg->route_set_frozen)
+	return;
+
+    /* Ignore if the message is an UPDATE response (see ticket #1781) */
+    if (pjsip_method_cmp(&rdata->msg_info.cseq->method, &update) == 0)
 	return;
 
     /* Only update route set if this message belongs to the same
