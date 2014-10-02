cede6c06f2c0f9349545260c1436a6f253585f7c
Sauw Ming <ming@teluu.com>
2014-10-02 02:45:33 +0000

    Re #1782 (misc): Fixed bug caused by wrong order of code made in r4933 (thanks to Brian Walker for noticing this)
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@4937 74dad513-b988-da41-8d7b-12977e46ad98

--- pjsip/src/pjsip/sip_transaction.c
+++ pjsip/src/pjsip/sip_transaction.c
@@ -2165,8 +2165,8 @@ PJ_DEF(pj_status_t) pjsip_tsx_retransmit_no_state(pjsip_transaction *tsx,
 
     pj_grp_lock_acquire(tsx->grp_lock);
     if (tdata == NULL) {
-        pjsip_tx_data_add_ref(tdata);
 	tdata = tsx->last_tx;
+	pjsip_tx_data_add_ref(tdata);
     }
     status = tsx_send_msg(tsx, tdata);
     pj_grp_lock_release(tsx->grp_lock);
