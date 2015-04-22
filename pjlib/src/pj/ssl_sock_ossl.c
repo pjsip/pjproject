fd4d53641c1e8f9e437431b99c4a9514b0ba3515
Sauw Ming <ming@teluu.com>
2015-04-22 03:43:00 +0000

    Re #1843: Support compilation with OpenSSL without OpenSSLv3 support
    Thanks to Alexander Traud for the patch.
    
    
    
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@5074 74dad513-b988-da41-8d7b-12977e46ad98

--- pjlib/src/pj/ssl_sock_ossl.c
+++ pjlib/src/pj/ssl_sock_ossl.c
@@ -334,8 +334,10 @@ static pj_status_t init_openssl(void)
 	meth = (SSL_METHOD*)SSLv23_server_method();
 	if (!meth)
 	    meth = (SSL_METHOD*)TLSv1_server_method();
+#ifndef OPENSSL_NO_SSL3_METHOD
 	if (!meth)
 	    meth = (SSL_METHOD*)SSLv3_server_method();
+#endif
 #ifndef OPENSSL_NO_SSL2
 	if (!meth)
 	    meth = (SSL_METHOD*)SSLv2_server_method();
@@ -530,8 +532,10 @@ static pj_status_t create_ssl(pj_ssl_sock_t *ssock)
 	ssl_method = (SSL_METHOD*)SSLv2_method();
 	break;
 #endif
+#ifndef OPENSSL_NO_SSL3_METHOD
     case PJ_SSL_SOCK_PROTO_SSL3:
 	ssl_method = (SSL_METHOD*)SSLv3_method();
+#endif
 	break;
     }
 
