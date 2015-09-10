e2c345747f28545a31533acc4737fe37714c175f
Nanang Izzuddin <nanang@teluu.com>
2015-09-10 07:17:22 +0000

    Fix #1884: Fixed silence frame length in stream (frame length calculation didn't include frame per packet).
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@5174 74dad513-b988-da41-8d7b-12977e46ad98

--- pjmedia/src/pjmedia/stream.c
+++ pjmedia/src/pjmedia/stream.c
@@ -1268,18 +1268,13 @@ static pj_status_t put_frame_imp( pjmedia_port *port,
 	       frame->buf == NULL &&
 	       stream->port.info.fmt.id == PJMEDIA_FORMAT_L16 &&
 	       (stream->dir & PJMEDIA_DIR_ENCODING) &&
-	       stream->codec_param.info.frm_ptime *
-	       stream->codec_param.info.channel_cnt *
-	       stream->codec_param.info.clock_rate/1000 <
-		  PJ_ARRAY_SIZE(zero_frame))
+	       stream->enc_samples_per_pkt < PJ_ARRAY_SIZE(zero_frame))
     {
 	pjmedia_frame silence_frame;
 
 	pj_bzero(&silence_frame, sizeof(silence_frame));
 	silence_frame.buf = zero_frame;
-	silence_frame.size = stream->codec_param.info.frm_ptime * 2 *
-			     stream->codec_param.info.channel_cnt *
-			     stream->codec_param.info.clock_rate / 1000;
+	silence_frame.size = stream->enc_samples_per_pkt * 2;
 	silence_frame.type = PJMEDIA_FRAME_TYPE_AUDIO;
 	silence_frame.timestamp.u32.lo = pj_ntohl(stream->enc->rtp.out_hdr.ts);
 
