668b97ff1577ad145bcec99fdf8cc388831a8ec3
Sauw Ming <ming@teluu.com>
2016-10-07 01:42:52 +0000

    Re #1945 (misc): Fixed incorrect usage of pj_memcmp()
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@5450 74dad513-b988-da41-8d7b-12977e46ad98

--- pjsip/src/pjsip/sip_msg.c
+++ pjsip/src/pjsip/sip_msg.c
@@ -251,7 +251,7 @@ PJ_DEF(void) pjsip_method_init_np(pjsip_method *m,
 {
     unsigned i;
     for (i=0; i<PJ_ARRAY_SIZE(method_names); ++i) {
-	if (pj_memcmp(str->ptr, method_names[i]->ptr, str->slen)==0 || 
+	if (pj_memcmp(str, &method_names[i], sizeof(pj_str_t))==0 || 
 	    pj_stricmp(str, method_names[i])==0) 
 	{
 	    m->id = (pjsip_method_e)i;
