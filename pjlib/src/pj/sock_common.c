812618e1f8d215153a50021f2247fd64d336c8b3
Sauw Ming <ming@teluu.com>
2016-11-17 04:38:25 +0000

    Fixed #1979: Add support to parse address string with scope ID
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@5485 74dad513-b988-da41-8d7b-12977e46ad98

--- pjlib/src/pj/sock_common.c
+++ pjlib/src/pj/sock_common.c
@@ -172,8 +172,12 @@ PJ_DEF(pj_status_t) pj_sockaddr_set_str_addr(int af,
     PJ_SOCKADDR_RESET_LEN(addr);
 
     if (str_addr && str_addr->slen) {
+#if defined(PJ_SOCKADDR_USE_GETADDRINFO) && PJ_SOCKADDR_USE_GETADDRINFO!=0
+	if (1) {
+#else
 	status = pj_inet_pton(PJ_AF_INET6, str_addr, &addr->ipv6.sin6_addr);
 	if (status != PJ_SUCCESS) {
+#endif
     	    pj_addrinfo ai;
 	    unsigned count = 1;
 
@@ -181,6 +185,7 @@ PJ_DEF(pj_status_t) pj_sockaddr_set_str_addr(int af,
 	    if (status==PJ_SUCCESS) {
 		pj_memcpy(&addr->ipv6.sin6_addr, &ai.ai_addr.ipv6.sin6_addr,
 			  sizeof(addr->ipv6.sin6_addr));
+		addr->ipv6.sin6_scope_id = ai.ai_addr.ipv6.sin6_scope_id;
 	    }
 	}
     } else {
