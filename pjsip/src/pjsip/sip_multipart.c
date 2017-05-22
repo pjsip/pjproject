595cac851ba7669367c4fbcc4a355c1fa97d2d7c
Sauw Ming <ming@teluu.com>
2017-05-22 03:53:35 +0000

    Fixed #2017: Incorrect parsing of zero length multipart body parts
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@5594 74dad513-b988-da41-8d7b-12977e46ad98

--- pjsip/src/pjsip/sip_multipart.c
+++ pjsip/src/pjsip/sip_multipart.c
@@ -653,13 +653,18 @@ PJ_DEF(pjsip_msg_body*) pjsip_multipart_parse(pj_pool_t *pool,
 
 	end_body = curptr;
 
-	/* The newline preceeding the delimiter is conceptually part of
-	 * the delimiter, so trim it from the body.
+	/* Note that when body is empty, end_body will be equal
+	 * to start_body.
 	 */
-	if (*(end_body-1) == '\n')
-	    --end_body;
-	if (*(end_body-1) == '\r')
-	    --end_body;
+        if (end_body > start_body) {
+	    /* The newline preceeding the delimiter is conceptually part of
+	     * the delimiter, so trim it from the body.
+	     */
+	    if (*(end_body-1) == '\n')
+	        --end_body;
+	    if (*(end_body-1) == '\r')
+	        --end_body;
+        }
 
 	/* Now that we have determined the part's boundary, parse it
 	 * to get the header and body part of the part.
