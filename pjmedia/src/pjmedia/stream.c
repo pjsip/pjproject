93c3c1bd290e40aae9b0f3d1ecade90de269f9d4
Riza Sulistyo <riza@teluu.com>
2017-10-06 06:54:37 +0000

    Re #2045(misc): To prevent assertion, don't copy rem_rtcp on pjmedia_stream_create()/pjmedia_vid_stream_create() if address is not set. Thanks to Lauri Vanksa for the Report.
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@5671 74dad513-b988-da41-8d7b-12977e46ad98

--- pjmedia/src/pjmedia/stream.c
+++ pjmedia/src/pjmedia/stream.c
@@ -2348,11 +2348,13 @@ PJ_DEF(pj_status_t) pjmedia_stream_create( pjmedia_endpt *endpt,
 	stream->out_rtcp_pkt_size = PJMEDIA_MAX_MTU;
 
     stream->out_rtcp_pkt = pj_pool_alloc(pool, stream->out_rtcp_pkt_size);
+    pj_bzero(&att_param, sizeof(att_param));
     att_param.stream = stream;
     att_param.media_type = PJMEDIA_TYPE_AUDIO;
     att_param.user_data = stream;
     pj_sockaddr_cp(&att_param.rem_addr, &info->rem_addr);
-    pj_sockaddr_cp(&att_param.rem_rtcp, &info->rem_rtcp);
+    if (pj_sockaddr_has_addr(&info->rem_rtcp.addr))
+	pj_sockaddr_cp(&att_param.rem_rtcp, &info->rem_rtcp);
     att_param.addr_len = pj_sockaddr_get_len(&info->rem_addr);
     att_param.rtp_cb = &on_rx_rtp;
     att_param.rtcp_cb = &on_rx_rtcp;
