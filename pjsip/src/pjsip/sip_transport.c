39b6c288775b07a944750902742a956e1662c93e
Sauw Ming <ming@teluu.com>
2017-11-10 03:21:52 +0000

    Fixed #2061: Unable to destroy certain PJSIP transports
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@5690 74dad513-b988-da41-8d7b-12977e46ad98

--- pjsip/src/pjsip/sip_transport.c
+++ pjsip/src/pjsip/sip_transport.c
@@ -974,7 +974,21 @@ static pj_bool_t is_transport_valid(pjsip_transport *tp, pjsip_tpmgr *tpmgr,
 				    const pjsip_transport_key *key,
 				    int key_len)
 {
-    return (pj_hash_get(tpmgr->table, key, key_len, NULL) == (void*)tp);
+    transport *tp_iter;
+
+    if (pj_hash_get(tpmgr->table, key, key_len, NULL) == (void*)tp) {
+        return PJ_TRUE;
+    }
+
+    tp_iter = tpmgr->tp_list.next;
+    while (tp_iter != &tpmgr->tp_list) {
+        if (tp_iter->tp == tp) {
+            return PJ_TRUE;
+        }
+        tp_iter = tp_iter->next;
+    }
+
+    return PJ_FALSE;
 }
 
 /*
