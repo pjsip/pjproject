975aa1047a4dcc439315d9180c0f850e855c37d0
Riza Sulistyo <riza@teluu.com>
2017-11-14 11:08:11 +0000

    Fixed #2064: Fix return code in pjsip_find_msg().
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@5694 74dad513-b988-da41-8d7b-12977e46ad98

--- pjsip/src/pjsip/sip_parser.c
+++ pjsip/src/pjsip/sip_parser.c
@@ -834,13 +834,13 @@ PJ_DEF(pj_status_t) pjsip_find_msg( const char *buf, pj_size_t size,
 				  pj_bool_t is_datagram, pj_size_t *msg_size)
 {
 #if PJ_HAS_TCP
-    const char *hdr_end;
-    const char *body_start;
+    const char *volatile hdr_end;
+    const char *volatile body_start;
     const char *pos;
-    const char *line;
+    const char *volatile line;
     int content_length = -1;
     pj_str_t cur_msg;
-    pj_status_t status = PJ_SUCCESS;
+    pj_status_t status = PJSIP_EMISSINGHDR;
     const pj_str_t end_hdr = { "\n\r\n", 3};
 
     *msg_size = size;
