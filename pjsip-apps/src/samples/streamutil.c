0d71a76ab23d8f28bf41c221304e049f5946ce36
Sauw Ming <ming@teluu.com>
2018-11-07 23:28:36 +0000

    Re #2147 (misc): Add pjmedia_transport_media_stop() to streamutil sample app, for good practice since we now require media transport to be started since #2097
    git-svn-id: https://svn.pjsip.org/repos/pjproject/trunk@5908 74dad513-b988-da41-8d7b-12977e46ad98

--- pjsip-apps/src/samples/streamutil.c
+++ pjsip-apps/src/samples/streamutil.c
@@ -831,6 +831,7 @@ on_exit:
 	pjmedia_transport *tp;
 
 	tp = pjmedia_stream_get_transport(stream);
+	pjmedia_transport_media_stop(tp);
 	pjmedia_stream_destroy(stream);
 	
 	pjmedia_transport_close(tp);
