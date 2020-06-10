f45f2d435a25c6816921d1b47ee172be4e4d5be3
Riza Sulistyo <trengginas@users.noreply.github.com>
2020-06-10 10:42:53 +0700

    Allow demote SRTP call to RTP when using re-INVITE with PJSUA_CALL_REINIT_MEDIA (#2451)
    * - Allow demote SRTP call to RTP when using re-INVITE with
    PJSUA_CALL_REINIT_MEDIA
    - DTLS renegotiation when receiving re-INVITE with modified media
    transport address
    
    * Revert DTLS_DEBUG to 0.
    
    * modification based on comments.
    
    * modified comment.

--- pjsip/src/pjsua-lib/pjsua_media.c
+++ pjsip/src/pjsua-lib/pjsua_media.c
@@ -3030,6 +3030,7 @@ pj_status_t pjsua_media_channel_deinit(pjsua_call_id call_id)
 	    call_med->tp = call_med->tp_orig = NULL;
 	}
         call_med->tp_orig = NULL;
+        call_med->rem_srtp_use = PJMEDIA_SRTP_UNKNOWN;
     }
 
     pj_log_pop_indent();
