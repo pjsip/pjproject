4cbcbcef2c7b9b8af517cc643968868a53107648
Alex Kalmuk <alexkalmuk@gmail.com>
2021-01-06 22:13:34 +0300

    Fix ai_socktype to be zero when passed to getaddrinfo
    Previos version "pj_SOCK_DGRAM() | pj_SOCK_STREAM()" is equal to SOCK_RAW,
    so getaddrinfo() was resolving addresses to SOCK_RAW, not
    to SOCK_DGRAM and SOCK_STREAM as expected.

--- pjlib/src/pj/addr_resolv_sock.c
+++ pjlib/src/pj/addr_resolv_sock.c
@@ -178,7 +178,9 @@ PJ_DEF(pj_status_t) pj_getaddrinfo(int af, const pj_str_t *nodename,
     /* Call getaddrinfo() */
     pj_bzero(&hint, sizeof(hint));
     hint.ai_family = af;
-    hint.ai_socktype = pj_SOCK_DGRAM() | pj_SOCK_STREAM();
+    /* Zero value of ai_socktype means the implementation shall attempt
+     * to resolve the service name for all supported socket types */
+    hint.ai_socktype = 0;
 
     rc = getaddrinfo(nodecopy, NULL, &hint, &res);
     if (rc != 0)
@@ -191,6 +193,11 @@ PJ_DEF(pj_status_t) pj_getaddrinfo(int af, const pj_str_t *nodename,
 	/* Ignore unwanted address families */
 	if (af!=PJ_AF_UNSPEC && res->ai_family != af)
 	    continue;
+
+	if (res->ai_socktype != pj_SOCK_DGRAM()
+                    && res->ai_socktype != pj_SOCK_STREAM()) {
+	        continue;
+	}
 
 	/* Store canonical name (possibly truncating the name) */
 	if (res->ai_canonname) {
