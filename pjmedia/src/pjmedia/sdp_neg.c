daff4e7749ffb032cd5546af248680a41fb2a086
sauwming <ming@teluu.com>
2021-03-08 10:01:27 +0800

    Increase SDP version when receiving re-INVITE without SDP (#2646)

--- pjmedia/src/pjmedia/sdp_neg.c
+++ pjmedia/src/pjmedia/sdp_neg.c
@@ -447,7 +447,18 @@ PJ_DEF(pj_status_t) pjmedia_sdp_neg_send_local_offer( pj_pool_t *pool,
 	neg->state = PJMEDIA_SDP_NEG_STATE_LOCAL_OFFER;
 	neg->neg_local_sdp = pjmedia_sdp_session_clone(pool, 
 						       neg->active_local_sdp);
-	*offer = neg->active_local_sdp;
+
+#if PJMEDIA_SDP_NEG_COMPARE_BEFORE_INC_VERSION
+    	if (pjmedia_sdp_session_cmp(neg->neg_local_sdp, 
+    				    neg->initial_sdp, 0) != PJ_SUCCESS)
+    	{
+	    neg->neg_local_sdp->origin.version++;
+    	}    
+#else
+    	neg->neg_local_sdp->origin.version++;
+#endif
+
+	*offer = neg->neg_local_sdp;
 
     } else {
 	/* We assume that we're in STATE_LOCAL_OFFER.
