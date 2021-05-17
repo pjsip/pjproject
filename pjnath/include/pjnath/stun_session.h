f0ff5817d0647bdecd1ec99488db9378e304cf83
sauwming <ming@teluu.com>
2021-05-17 09:56:27 +0800

    Fix double free of stun session (#2709)

--- pjnath/include/pjnath/stun_session.h
+++ pjnath/include/pjnath/stun_session.h
@@ -341,6 +341,7 @@ struct pj_stun_tx_data
     pj_pool_t		*pool;		/**< Pool.			    */
     pj_stun_session	*sess;		/**< The STUN session.		    */
     pj_stun_msg		*msg;		/**< The STUN message.		    */
+    pj_bool_t		 is_destroying; /**< Is destroying?		    */
 
     void		*token;		/**< The token.			    */
 
