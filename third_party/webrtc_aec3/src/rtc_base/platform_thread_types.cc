a44fda3370b0c2ac56930e2e1226cd6d41df5eef
mingteluu <ming@teluu.com>
2021-05-31 18:03:25 +0800

    Android build for setup for armeabi-v7a, arm64-v8a, x86, and x86_64

--- third_party/webrtc_aec3/src/rtc_base/platform_thread_types.cc
+++ third_party/webrtc_aec3/src/rtc_base/platform_thread_types.cc
@@ -105,7 +105,7 @@ void SetCurrentThreadName(const char* name) {
   } __except (EXCEPTION_EXECUTE_HANDLER) {  // NOLINT
   }
 #pragma warning(pop)
-#elif defined(WEBRTC_LINUX) || defined(WEBRTC_ANDROID)
+#elif defined(WEBRTC_LINUX) // || defined(WEBRTC_ANDROID)
   prctl(PR_SET_NAME, reinterpret_cast<unsigned long>(name));  // NOLINT
 #elif defined(WEBRTC_MAC) || defined(WEBRTC_IOS)
   pthread_setname_np(name);
