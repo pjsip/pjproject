413142a2383ef4a438180e2b0cc6ef889208ad41
emretufekci <contact@emretufekci.com>
2021-06-04 13:53:45 +0300

    contributor name changes

--- pjsip-apps/src/pjsua2/ios-swift-pjsua2/ios-swift-pjsua2/Model/Model.swift
+++ pjsip-apps/src/pjsua2/ios-swift-pjsua2/ios-swift-pjsua2/Model/Model.swift
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2012-2012 Teluu Inc. (http://www.teluu.com)
- * Contributed by Regis Montoya (aka r3gis - www.r3gis.fr)
+ * Contributed by Emre Tufekci (github.com/emretufekci)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
