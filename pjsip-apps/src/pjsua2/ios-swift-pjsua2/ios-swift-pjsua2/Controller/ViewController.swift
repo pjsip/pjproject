0d4d41e86ccb8320f6c5e9b049dbf5caa9a91124
emretufekci <contact@emretufekci.com>
2021-06-09 15:40:33 +0300

    Done button for the keyboard

--- pjsip-apps/src/pjsua2/ios-swift-pjsua2/ios-swift-pjsua2/Controller/ViewController.swift
+++ pjsip-apps/src/pjsua2/ios-swift-pjsua2/ios-swift-pjsua2/Controller/ViewController.swift
@@ -44,8 +44,14 @@ class ViewController: UIViewController {
         
         //Listen incoming call via function pointer
         CPPWrapper().incoming_call_wrapper(incoming_call_swift)
-        
+     
+        //Done button to the keyboard
+        sipIpTField.addDoneButtonOnKeyboard()
+        sipPortTField.addDoneButtonOnKeyboard()
+        sipUsernameTField.addDoneButtonOnKeyboard()
+        sipPasswordTField.addDoneButtonOnKeyboard()
     }
+    
     
     //Refresh Button
     @IBAction func refreshStatus(_ sender: UIButton) {
@@ -153,4 +159,36 @@ class ViewController: UIViewController {
     }
     
     
+}
+extension UITextField{
+    @IBInspectable var doneAccessory: Bool{
+        get{
+            return self.doneAccessory
+        }
+        set (hasDone) {
+            if hasDone{
+                addDoneButtonOnKeyboard()
+            }
+        }
+    }
+
+    func addDoneButtonOnKeyboard()
+    {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+
+        self.inputAccessoryView = doneToolbar
+    }
+
+    @objc func doneButtonAction()
+    {
+        self.resignFirstResponder()
+    }
 }
