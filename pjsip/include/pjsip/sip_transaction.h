f5249ca70efa5e0c9695bf0d9574883ffb71ef3f
Krzysztof Marciniak <88203992+k-marciniak@users.noreply.github.com>
2022-02-14 05:49:29 +0100

    Added pjsip_tsx_set_timers to change timers at runtime (#2781)
    * Added pjsip_tsx_set_timers to change timers at runtime
    
    Added new function pjsip_tsx_set_timers in sip_transaction.c
    which allows to change session timers during runtime.
    It also allows to change timer values independently,
    currently all timers are set at various ratios from
    t1 during init. This was required for server which could
    change timeout configuration on runtime, but could be
    usable in other projects.
    
    * Add synchronization of timer values in pjsip_cfg().
    
    Add synchronization of timer values to pjsip_cfg() and another
    function to (re)synchronize timer values from pjsip_cfg(),
    `pjsip_tsx_initialize_timer_values()`.
    
    `pjsip_tsx_set_timers` now accepts zeroed arguments to mean
    that it should not change that timer value.

--- pjsip/include/pjsip/sip_transaction.h
+++ pjsip/include/pjsip/sip_transaction.h
@@ -435,6 +435,22 @@ PJ_DECL(pj_status_t) pjsip_tsx_stop_retransmit(pjsip_transaction *tsx);
 PJ_DECL(pj_status_t) pjsip_tsx_set_timeout(pjsip_transaction *tsx,
 					   unsigned millisec);
 
+/*
+ * Change timer values used by transaction layer. Currently scheduled
+ * timers will not be changed. Any value set to 0 will be left
+ * unchanged.
+ *
+ * @param t1 - Transaction T1 timeout, in msec
+ * @param t2 - Transaction T2 timeout, in msec
+ * @param t4 - Transaction completed timer for non-INVITE, in msec
+ * @param td - Transaction completed timer for INVITE, in msec
+ */
+PJ_DECL(void) pjsip_tsx_set_timers(unsigned t1, unsigned t2, unsigned t4, unsigned td);
+
+/*
+ * (Re)Initializes timer values from `pjsip_cfg()`.
+ */
+PJ_DECL(void) pjsip_tsx_initialize_timer_values(void);
 
 /**
  * Get the transaction instance in the incoming message. If the message
