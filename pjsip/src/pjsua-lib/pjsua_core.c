30714b6681e81055f3a4982c38a3dc33d9454c52
Riza Sulistyo <trengginas@users.noreply.github.com>
2022-03-07 14:34:13 +0700

    Use PJ_ASSERT_RETURN on pjsua_init_tpselector()

--- pjsip/src/pjsua-lib/pjsua_core.c
+++ pjsip/src/pjsua-lib/pjsua_core.c
@@ -3126,7 +3126,8 @@ void pjsua_init_tpselector(pjsua_transport_id tp_id,
     if (tp_id == PJSUA_INVALID_ID)
 	return;
 
-    pj_assert(tp_id >= 0 && tp_id < (int)PJ_ARRAY_SIZE(pjsua_var.tpdata));
+    PJ_ASSERT_RETURN(tp_id >= 0 && 
+		     tp_id < (int)PJ_ARRAY_SIZE(pjsua_var.tpdata), );
     tpdata = &pjsua_var.tpdata[tp_id];
 
     flag = pjsip_transport_get_flag_from_type(tpdata->type);
