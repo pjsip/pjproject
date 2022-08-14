5a92eca1df13cc033cc10b1073d7e8e6a046f7db
Nanang Izzuddin <nanang@teluu.com>
2022-08-14 15:08:43 +0700

    Add two VS2015 projects for CSharp: SWIG binding builder & sample app

---
+++ pjsip-apps/src/swig/csharp/sample/SimplePjsua2CS/Form1.Designer.cs
@@ -0,0 +1,51 @@
+
+namespace SimplePjsua2CS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ pjsip-apps/src/swig/csharp/sample/SimplePjsua2CS/Form1.cs
@@ -0,0 +1,34 @@
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SimplePjsua2CS
+{
+    public partial class Form1 : Form
+    {
+        private pjsua2xamarin.PjSample pj = new pjsua2xamarin.PjSample();
+        private Panel panel;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pj.start();
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            panel = new Panel();
+            panel.Size = new Size(350, 250);
+            panel.Location = new Point(20, 20);
+            Controls.Add(panel);
+
+            pj.startPreview(panel.Handle);
+            //pj.startPreview(System.IntPtr.Zero);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            pj.stopPreview();
+            pj.stop();
+        }
+    }
+}
