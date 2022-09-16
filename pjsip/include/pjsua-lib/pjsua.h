14674810fcedc80a6a102f6606e56bf267f1058a
SebaLedesma <sebastian_ledesma@yahoo.com>
2022-09-16 16:17:19 -0300

    Documentation fix, Media quality default val is 8

--- pjsip/include/pjsua-lib/pjsua.h
+++ pjsip/include/pjsua-lib/pjsua.h
@@ -6843,7 +6843,7 @@ struct pjsua_media_config
      * The media quality also sets speex codec quality/complexity to the
      * number.
      *
-     * Default: 5 (PJSUA_DEFAULT_CODEC_QUALITY).
+     * Default: 8 (PJSUA_DEFAULT_CODEC_QUALITY).
      */
     unsigned		quality;
 
