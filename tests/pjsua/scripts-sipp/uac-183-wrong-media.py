702b0d7126d785ffde7ed116038bd1abb3970f82
Alexey Vasilyev <alexei.vasilyev@gmail.com>
2022-11-03 14:47:53 +0100

    Fix changed behavior to cancel call in EARLY state
    Call in EARLY state should be cancelled when no media negotiated yet.
    Tests confirm right behavior.

---
+++ tests/pjsua/scripts-sipp/uac-183-wrong-media.py
@@ -0,0 +1,9 @@
+# $Id$
+#
+import inc_const as const
+
+PJSUA = ["--null-audio --max-calls=1 --no-tcp --auto-answer=183"]
+
+PJSUA_EXPECTS = [[0, "Response msg 488/INVITE", ""]]
+
+PJSUA_CLI_EXPECTS = [[0, "Response msg 488/INVITE", ""]]
