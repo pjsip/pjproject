f040389936e4fc5bdaf10d478ae40627ff2f358a
sauwming <ming@teluu.com>
2023-02-13 12:36:23 +0800

    Add video to iOS swift pjsua2 sample app

--- pjsip-apps/src/pjsua2/ios-swift-pjsua2/ios-swift-pjsua2/PJSua2/wrapper.h
+++ pjsip-apps/src/pjsua2/ios-swift-pjsua2/ios-swift-pjsua2/PJSua2/wrapper.h
@@ -40,40 +40,15 @@
 /**
  Create Account via following config(string username, string password, string ip, string port)
  */
--(void) createAccountWrapper :(NSString*) username :(NSString*) password :(NSString*) ip :(NSString*) port;
+-(void) createAccountWrapper :(NSString*) username :(NSString*) password
+                             :(NSString*) registrar :(NSString*) port;
 
 /**
  Unregister account
  */
 -(void) unregisterAccountWrapper;
 
-
-
-//Register State Info
-
--(bool) registerStateInfoWrapper;
-
-
-
 //Call
-/**
- Get caller id for incoming call, checks account currently registered (ai.regIsActive)
- */
--(NSString*) incomingCallInfoWrapper;
-
-
-/**
- Listener (When we have incoming call, this function pointer will notify swift.)
- (Runs swift code from C++)
- */
--(void) incoming_call_wrapper: (void(*)())function;
-
-/**
- Listener (When we have changes on the call state, this function pointer will notify swift.)
- (Runs swift code from C++)
- */
--(void) call_listener_wrapper: (void(*)(int))function;
-
 /**
  Answer incoming call
  */
@@ -95,7 +70,39 @@
 -(void) unholdCallWrapper;
 
 /**
- Make outgoing call (string dest_uri) -> e.g. makeCall(sip:<SIP_USERNAME@SIP_IP:SIP_PORT>)
+ Make outgoing call (string dest_uri) -> e.g. makeCall(sip:<SIP_USERNAME@SIP_URI:SIP_PORT>)
  */
 -(void) outgoingCallWrapper :(NSString*) dest_uri;
+
+/**
+ Get caller id for incoming call, checks account currently registered (ai.regIsActive)
+ */
+-(NSString*) incomingCallInfoWrapper;
+
+//Listener
+/**
+ Listener (When we have incoming call, this function pointer will notify swift.)
+ (Runs swift code from C++)
+ */
+-(void) incoming_call_wrapper: (void(*)())function;
+
+/**
+ Listener (When we have changes on the call state, this function pointer will notify swift.)
+ (Runs swift code from C++)
+ */
+-(void) call_listener_wrapper: (void(*)(int))function;
+
+/**
+ Listener (When we have changes on the acc reg state, this function pointer will notify swift.)
+ (Runs swift code from C++)
+ */
+-(void) acc_listener_wrapper: (void(*)(bool))function;
+
+/**
+ Listener (When we have video, this function pointer will notify swift.)
+ (Runs swift code from C++)
+ */
+-(void) update_video_wrapper: (void(*)(void *))function;
+
+
 @end
