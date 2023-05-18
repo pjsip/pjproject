08898cf72a6d6e98febb83e9c716177d8bd028d2
Vitold Sedyshev <vsedyshev@intermedia.net>
2023-05-18 22:56:18 +0300

    HPBX-12077 Make FEC statistics

--- pjsip/src/pjsua-lib/pjsua_aud.c
+++ pjsip/src/pjsua-lib/pjsua_aud.c
@@ -210,8 +210,9 @@ PJ_DEF(pj_status_t) pjsua_call_get_stream_stat( pjsua_call_id call_id,
             status = pjmedia_stream_get_stat_jbuf(call_med->strm.a.stream,
                                                   &stat->jbuf);
 #if defined(PJMEDIA_HAS_OPUS_CODEC) && (PJMEDIA_HAS_OPUS_CODEC!=0)
-        status2 = pjmedia_stream_get_stat_codec(call_med->strm.a.stream,
-                                                &stat->opus_stat);
+        pjmedia_stream *stream = call_med->strm.a.stream;
+        pjmedia_codec *codec = pjmedia_stream_get_codec(stream);
+        status2 = pjmedia_codec_opus_get_stat(codec, &stat->opus_stat);
         if (status2 != PJ_SUCCESS) {
             PJ_LOG(3, (THIS_FILE, "Unable to obtain OPUS codec statistics!"));
         }
