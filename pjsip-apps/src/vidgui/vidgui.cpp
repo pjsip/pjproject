77d2a6ef99a44fa86caa0bf934df82ad8ad75d43
Shubham Fuloria <sfuloria10200@gmail.com>
2023-07-12 01:26:08 +0530

    redeclared variable error fixed

--- pjsip-apps/src/vidgui/vidgui.cpp
+++ pjsip-apps/src/vidgui/vidgui.cpp
@@ -658,7 +658,7 @@ static void simple_registrar(pjsip_rx_data *rdata)
     srv->hvalue = pj_str((char*)"pjsua simple registrar");
     pjsip_msg_add_hdr(tdata->msg, (pjsip_hdr*)srv);
 
-    pj_status_t status = pjsip_endpt_send_response2(pjsua_get_pjsip_endpt(),
+    status = pjsip_endpt_send_response2(pjsua_get_pjsip_endpt(),
                                rdata, tdata, NULL, NULL);
     if (status != PJ_SUCCESS) pjsip_tx_data_dec_ref(tdata);
 }
