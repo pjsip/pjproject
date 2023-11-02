3d2fb8597ddf47b52ede6eb9a0a8cf07454d1bd9
Brad Smith <brad@comstyle.com>
2023-11-02 02:44:43 -0400

    sdp.c: don't pass NULL pointer to log string format
    Passing a NULL pointer to log string format will result in a crash on OpenBSD.
    
    https://flak.tedunangst.com/post/its-hard-work-printing-nothing

--- pjmedia/src/pjmedia/sdp.c
+++ pjmedia/src/pjmedia/sdp.c
@@ -611,13 +611,13 @@ PJ_DEF(pj_status_t) pjmedia_sdp_rtpmap_to_attr(pj_pool_t *pool,
     len = pj_ansi_snprintf(tempbuf, sizeof(tempbuf), 
                            "%.*s %.*s/%u%s%.*s",
                            (int)rtpmap->pt.slen,
-                           rtpmap->pt.ptr,
+                           rtpmap->pt.ptr ? rtpmap->pt.ptr : "(null)",
                            (int)rtpmap->enc_name.slen,
-                           rtpmap->enc_name.ptr,
+                           rtpmap->enc_name.ptr ? rtpmap->enc_name.ptr : "(null)",
                            rtpmap->clock_rate,
                            (rtpmap->param.slen ? "/" : ""),
                            (int)rtpmap->param.slen,
-                           rtpmap->param.ptr);
+                           rtpmap->param.ptr ? rtpmap->param.ptr : "(null)");
 
     if (len < 1 || len >= (int)sizeof(tempbuf))
         return PJMEDIA_SDP_ERTPMAPTOOLONG;
