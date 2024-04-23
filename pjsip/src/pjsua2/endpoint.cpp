e463d84b860ec9f0f8da17993f301045db8e05c5
sauwming <ming@teluu.com>
2024-04-23 13:33:48 +0800

    Throw PJ_ENOTSUP error

--- pjsip/src/pjsua2/endpoint.cpp
+++ pjsip/src/pjsua2/endpoint.cpp
@@ -2493,32 +2493,36 @@ void Endpoint::codecSetParam(const string &codec_id,
 
 CodecOpusConfig Endpoint::getCodecOpusConfig() const PJSUA2_THROW(Error)
 {
-   CodecOpusConfig config;
+    CodecOpusConfig config;
 #if defined(PJMEDIA_HAS_OPUS_CODEC) && (PJMEDIA_HAS_OPUS_CODEC!=0)
-   pjmedia_codec_opus_config opus_cfg;
+    pjmedia_codec_opus_config opus_cfg;
 
-   PJSUA2_CHECK_EXPR(pjmedia_codec_opus_get_config(&opus_cfg));
-   config.fromPj(opus_cfg);
+    PJSUA2_CHECK_EXPR(pjmedia_codec_opus_get_config(&opus_cfg));
+    config.fromPj(opus_cfg);
+#else
+    PJSUA2_RAISE_ERROR(PJ_ENOTSUP);
 #endif
 
-   return config;
+    return config;
 }
 
 void Endpoint::setCodecOpusConfig(const CodecOpusConfig &opus_cfg)
                                   PJSUA2_THROW(Error)
 {
 #if defined(PJMEDIA_HAS_OPUS_CODEC) && (PJMEDIA_HAS_OPUS_CODEC!=0)
-   const pj_str_t codec_id = {(char *)"opus", 4};
-   pjmedia_codec_param param;
-   pjmedia_codec_opus_config new_opus_cfg;
-
-   PJSUA2_CHECK_EXPR(pjsua_codec_get_param(&codec_id, &param));
-   new_opus_cfg = opus_cfg.toPj();
-
-   PJSUA2_CHECK_EXPR(pjmedia_codec_opus_set_default_param(&new_opus_cfg,
-                                                          &param));
+    const pj_str_t codec_id = {(char *)"opus", 4};
+    pjmedia_codec_param param;
+    pjmedia_codec_opus_config new_opus_cfg;
+    
+    PJSUA2_CHECK_EXPR(pjsua_codec_get_param(&codec_id, &param));
+    new_opus_cfg = opus_cfg.toPj();
+    
+    PJSUA2_CHECK_EXPR(pjmedia_codec_opus_set_default_param(&new_opus_cfg,
+                                                           &param));
 #else
     PJ_UNUSED_ARG(opus_cfg);
+
+    PJSUA2_RAISE_ERROR(PJ_ENOTSUP);
 #endif
 }
 
