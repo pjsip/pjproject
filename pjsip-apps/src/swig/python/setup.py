203193b2445b029f8fb00bcca1b979a565a6e34e
Riza Sulistyo <trengginas@users.noreply.github.com>
2024-08-30 10:55:44 +0700

    Use setuptools to replace distutils

--- pjsip-apps/src/swig/python/setup.py
+++ pjsip-apps/src/swig/python/setup.py
@@ -17,7 +17,11 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
 #
-from distutils.core import setup, Extension
+try:
+   from setuptools import setup, Extension
+except ImportError:
+   from distutils.core import setup, Extension
+
 import os
 import sys
 import platform
