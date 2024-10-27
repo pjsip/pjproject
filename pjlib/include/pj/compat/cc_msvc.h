24c959f54599cd7d13476bd2cb8d0c1b59594696
Leonid Goltsblat <lgoltsblat@gmail.com>
2024-10-27 23:43:09 +0100

    untabify

--- pjlib/include/pj/compat/cc_msvc.h
+++ pjlib/include/pj/compat/cc_msvc.h
@@ -28,10 +28,10 @@
 #  error "This header file is only for Visual C compiler!"
 #endif
 
-#define PJ_CC_NAME	    "msvc"
-#define PJ_CC_VER_1	    (_MSC_VER/100)
-#define PJ_CC_VER_2	    (_MSC_VER%100)
-#define PJ_CC_VER_3	    0
+#define PJ_CC_NAME          "msvc"
+#define PJ_CC_VER_1         (_MSC_VER/100)
+#define PJ_CC_VER_2         (_MSC_VER%100)
+#define PJ_CC_VER_3         0
 
 /* Disable CRT deprecation warnings. */
 #if PJ_CC_VER_1 >= 8 && !defined(_CRT_SECURE_NO_DEPRECATE)
@@ -55,30 +55,30 @@
 #endif
 
 #ifdef __cplusplus
-#  define PJ_INLINE_SPECIFIER	inline
+#  define PJ_INLINE_SPECIFIER   inline
 #else
-#  define PJ_INLINE_SPECIFIER	static __inline
+#  define PJ_INLINE_SPECIFIER   static __inline
 #endif
 
 #define PJ_EXPORT_DECL_SPECIFIER    __declspec(dllexport)
-#define PJ_EXPORT_DEF_SPECIFIER	    __declspec(dllexport)
+#define PJ_EXPORT_DEF_SPECIFIER     __declspec(dllexport)
 #define PJ_IMPORT_DECL_SPECIFIER    __declspec(dllimport)
 
-#define PJ_THREAD_FUNC	
-#define PJ_NORETURN		__declspec(noreturn)
-#define PJ_ATTR_NORETURN	
-#define PJ_ATTR_MAY_ALIAS	
+#define PJ_THREAD_FUNC  
+#define PJ_NORETURN             __declspec(noreturn)
+#define PJ_ATTR_NORETURN        
+#define PJ_ATTR_MAY_ALIAS       
 
-#define PJ_HAS_INT64	1
+#define PJ_HAS_INT64    1
 
 typedef __int64 pj_int64_t;
 typedef unsigned __int64 pj_uint64_t;
 
-#define PJ_INT64(val)		val##i64
-#define PJ_UINT64(val)		val##ui64
-#define PJ_INT64_FMT		"I64"
+#define PJ_INT64(val)           val##i64
+#define PJ_UINT64(val)          val##ui64
+#define PJ_INT64_FMT            "I64"
 
-#define PJ_UNREACHED(x)	    	
+#define PJ_UNREACHED(x)         
 
 //#define PJ_ALIGN_DATA(declaration, alignment) __declspec(align(alignment)) declaration
 //#pragma warning(disable:4324)   // structure padded due to align()
@@ -92,5 +92,5 @@ typedef unsigned __int64 pj_uint64_t;
 #define PJ_SYS_ALIGN_SUFFIX PJ_ALIGN_DATA_SUFFIX(MEMORY_ALLOCATION_ALIGNMENT)
 
 
-#endif	/* __PJ_COMPAT_CC_MSVC_H__ */
+#endif  /* __PJ_COMPAT_CC_MSVC_H__ */
 
--- pjlib/src/pjlib-test/test.c
+++ pjlib/src/pjlib-test/test.c
@@ -32,14 +32,14 @@
 
 #endif
 
-#define DO_TEST(test)	do { \
-			    PJ_LOG(3, ("test", "Running %s...", #test));  \
-			    rc = test; \
-			    PJ_LOG(3, ("test",  \
-				       "%s(%d)",  \
-				       (rc ? "..ERROR" : "..success"), rc)); \
-			    if (rc!=0) goto on_return; \
-			} while (0)
+#define DO_TEST(test)   do { \
+                            PJ_LOG(3, ("test", "Running %s...", #test));  \
+                            rc = test; \
+                            PJ_LOG(3, ("test",  \
+                                       "%s(%d)",  \
+                                       (rc ? "..ERROR" : "..success"), rc)); \
+                            if (rc!=0) goto on_return; \
+                        } while (0)
 
 
 pj_pool_factory *mem;
@@ -48,7 +48,7 @@ int param_echo_sock_type;
 const char *param_echo_server = ECHO_SERVER_ADDRESS;
 int param_echo_port = ECHO_SERVER_START_PORT;
 int param_log_decor = PJ_LOG_HAS_NEWLINE | PJ_LOG_HAS_TIME |
-		      PJ_LOG_HAS_MICRO_SEC | PJ_LOG_HAS_INDENT;
+                      PJ_LOG_HAS_MICRO_SEC | PJ_LOG_HAS_INDENT;
 pj_bool_t param_ci_mode = PJ_FALSE;  /* GH CI mode: more lenient tests */
 
 int null_func()
@@ -82,8 +82,8 @@ int test_inner(void)
 
     rc = pj_init();
     if (rc != 0) {
-	app_perror("pj_init() error!!", rc);
-	return rc;
+        app_perror("pj_init() error!!", rc);
+        return rc;
     }
 
     pj_dump_config();
@@ -112,7 +112,7 @@ int test_inner(void)
 #if INCLUDE_LIST_TEST
     DO_TEST( list_test() );
 #endif
-    
+
 #if INCLUDE_STACK_TEST
     DO_TEST(stack_test());
 #endif
@@ -235,16 +235,16 @@ on_return:
 
     pj_caching_pool_destroy( &caching_pool );
 
-    PJ_LOG(3,("test", ""));
+    PJ_LOG(3,("test", " "));
 
     pj_thread_get_stack_info(pj_thread_this(), &filename, &line);
     PJ_LOG(3,("test", "Stack max usage: %u, deepest: %s:%u",
-	              pj_thread_get_stack_max_usage(pj_thread_this()),
-		      filename, line));
+                      pj_thread_get_stack_max_usage(pj_thread_this()),
+                      filename, line));
     if (rc == 0)
-	PJ_LOG(3,("test", "Looks like everything is okay!.."));
+        PJ_LOG(3,("test", "Looks like everything is okay!.."));
     else
-	PJ_LOG(3,("test", "Test completed with error(s)"));
+        PJ_LOG(3,("test", "Test completed with error(s)"));
 
     pj_shutdown();
 
