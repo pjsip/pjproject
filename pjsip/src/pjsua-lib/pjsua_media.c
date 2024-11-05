b12eff8bf2a9f21c7d4f1fc006f3c1af0e2b097f
Soroush mohammadi <sorooshm78@gmail.com>
2024-11-05 10:58:20 +0000

    refactor

--- pjsip/src/pjsua-lib/pjsua_media.c
+++ pjsip/src/pjsua-lib/pjsua_media.c
@@ -1951,11 +1951,11 @@ static pj_status_t call_media_init_cb(pjsua_call_media *call_med,
         goto on_return;
     }
 
-	/* Check if media is deinitializing */
-	if (call_med->call->async_call.med_ch_deinit || !call_med->tp) {
-		status = PJ_ECANCELLED;
-		goto on_return;
-	}
+    /* Check if media is deinitializing */
+    if (call_med->call->async_call.med_ch_deinit || !call_med->tp) {
+       status = PJ_ECANCELLED;
+       goto on_return;
+    }
 
     pjmedia_transport_simulate_lost(call_med->tp, PJMEDIA_DIR_ENCODING,
                                     pjsua_var.media_cfg.tx_drop_pct);
