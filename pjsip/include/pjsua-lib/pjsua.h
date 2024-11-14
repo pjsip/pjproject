f5d0bd0d164c79a6b6f4e07d7edc4c23e90f162a
sauwming <ming@teluu.com>
2024-11-14 16:26:40 +0800

    Revert "Fixed doc for sending DTMF (#4099)" (#4160)
    This reverts commit e289ddf3d80c61ff5ec37ce4ca49a24997103a16.

--- pjsip/include/pjsua-lib/pjsua.h
+++ pjsip/include/pjsua-lib/pjsua.h
@@ -5623,8 +5623,7 @@ typedef struct pjsua_call_send_dtmf_param
     pjsua_dtmf_method method;
 
     /**
-     * The signal duration used for the DTMF. This field is only used
-     * if the method is PJSUA_DTMF_METHOD_SIP_INFO.
+     * The signal duration used for the DTMF.
      *
      * Default: PJSUA_CALL_SEND_DTMF_DURATION_DEFAULT
      */
