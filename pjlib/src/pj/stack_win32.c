6fefbf5d3177dcfd9727b9fcaed930c197e3b1c9
Leonid Goltsblat <lgoltsblat@gmail.com>
2024-12-11 19:35:20 +0100

    copyright info added

--- pjlib/src/pj/stack_win32.c
+++ pjlib/src/pj/stack_win32.c
@@ -1,3 +1,21 @@
+/* 
+* Copyright (C) 2008-2024 Teluu Inc. (http://www.teluu.com)
+* Copyright (C) 2022-2024 Leonid Goltsblat <lgoltsblat@gmail.com>
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+*/
 #include <pj/os.h>
 #include <pj/assert.h>
 #include <pj/log.h>
@@ -17,7 +35,7 @@
  */
 
 #ifndef PJ_WIN32
-#error stack_win32.c should be compiled înly for Windows platform
+#error stack_win32.c should be compiled only for Windows platform
 #endif  // PJ_WIN32
 
 #include <windows.h>
