b04175a876dc8c7d7cb8bddfab151387b9868d10
Soroush mohammadi <sorooshm78@gmail.com>
2024-12-24 10:42:02 +0000

    Normalize line endings

--- pjsip/src/pjsua-lib/pjsua_media.c
+++ pjsip/src/pjsua-lib/pjsua_media.c
@@ -3003,16 +3003,6 @@ pj_status_t pjsua_media_channel_create_sdp(pjsua_call_id call_id,
             continue;
         }
 
-        /* Check if request supports PJSIP_INV_REQUIRE_SIPREC. If so Get label
-         * attribute in SDP offer and add label attribute to SDP answer
-         */
-        if (call->inv && (call->inv->options & PJSIP_INV_REQUIRE_SIPREC)) {
-            pjmedia_sdp_attr *label_attr;
-            label_attr = pjmedia_sdp_attr_get_label(rem_sdp->media[mi]);
-            m->attr[m->attr_count++] = pjmedia_sdp_attr_create_label(pool, 
-                                                                    &label_attr->value);
-        }
-
         /* Add ssrc and cname attribute */
         m->attr[m->attr_count++] = pjmedia_sdp_attr_create_ssrc(pool,
                                                                 call_med->ssrc,
