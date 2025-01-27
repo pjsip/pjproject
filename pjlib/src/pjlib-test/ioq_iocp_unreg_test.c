c26067d9817faf61db8dde5c8357969273d47de6
Nanang Izzuddin <nanang@teluu.com>
2025-01-27 11:54:43 +0700

    update copyright line order

--- pjlib/src/pjlib-test/ioq_iocp_unreg_test.c
+++ pjlib/src/pjlib-test/ioq_iocp_unreg_test.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2024 jimying at github dot com.
  * Copyright (C) 2024 Teluu Inc. (http://www.teluu.com)
+ * Copyright (C) 2024 jimying at github dot com.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
