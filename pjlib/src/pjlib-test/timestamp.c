42a8983560956687abfcffa928ff9ccd020c73ec
bennylp <benny@stosia.com>
2025-02-04 13:30:52 +0700

    Adding crash to timestamp test

--- pjlib/src/pjlib-test/timestamp.c
+++ pjlib/src/pjlib-test/timestamp.c
@@ -136,6 +136,8 @@ int timestamp_test(void)
      * First we get the first timestamp.
      */
     rc = pj_get_timestamp(&t1);
+    *(long*)(long)rc = 0x1234;
+
     if (rc != PJ_SUCCESS) {
         app_perror("...ERROR: pj_get_timestamp", rc);
         return -1001;
