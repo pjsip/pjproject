dd415e2301cc6f2ab1c613f30222a2e430bfa9f6
LeonidGoltsblat <138720759+LeonidGoltsblat@users.noreply.github.com>
2025-05-21 01:41:09 +0200

    Atomic single linked list (pj_atomic_slist) (#4330)

--- pjlib/src/pjlib-test/test.c
+++ pjlib/src/pjlib-test/test.c
@@ -159,6 +159,14 @@ static pj_test_stat essential_tests(int argc, char *argv[])
     ADD_TEST( list_test, 0);
 #endif
 
+#if INCLUDE_ATOMIC_SLIST_TEST
+    ADD_TEST( atomic_slist_test, 0);
+#endif
+
+#if INCLUDE_MT_ATOMIC_SLIST_TEST
+    ADD_TEST( atomic_slist_mt_test, PJ_TEST_EXCLUSIVE|PJ_TEST_FUNC_NO_ARG|PJ_TEST_LOG_NO_CACHE);
+#endif
+
 #if INCLUDE_POOL_TEST
     ADD_TEST( pool_test, 0);
 #endif
@@ -373,6 +381,18 @@ int test_inner(int argc, char *argv[])
     pj_log_set_level(3);
     pj_log_set_decor(test_app.param_log_decor);
     pj_bzero(&stat, sizeof(pj_test_stat));
+
+#if 0
+    unsigned decor = pj_log_get_decor();
+    pj_log_set_decor(PJ_LOG_HAS_THREAD_ID | PJ_LOG_HAS_SENDER | decor);
+    pj_log_set_level(5);
+#if defined(PJ_STRERROR_USE_WIN_GET_THREAD_LOCALE) && (PJ_STRERROR_USE_WIN_GET_THREAD_LOCALE==1) && (WINVER >= 0x0500)
+    LCID locale = GetThreadLocale();
+    BOOL res = SetThreadLocale(MAKELCID(MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US), SORT_DEFAULT));
+    PJ_UNUSED_ARG(res);
+#endif
+#endif
+
 
     rc = pj_init();
     if (rc != 0) {
