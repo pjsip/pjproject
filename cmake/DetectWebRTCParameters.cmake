bf4a18ae4c51e1d78a077c91dbff129481c8f416
Ayman Al-Qadhi <alqd@protonmail.com>
2025-07-07 22:55:47 +0300

    Make WebRTC parameters use variables instead of an `INTERFACE` target
    Although an `INTERFACE` target would have have a better approach, this
    mandates that taget gets exported.

--- cmake/DetectWebRTCParameters.cmake
+++ cmake/DetectWebRTCParameters.cmake
@@ -1,41 +1,32 @@
-set(OPTS_TARGET webrtc_options)
-
-if(TARGET ${OPTS_TARGET})
-  return()
-endif()
-
-add_library(${OPTS_TARGET} INTERFACE)
-add_library(WebRTC::Options ALIAS ${OPTS_TARGET})
-
-#
-# Configuration
-#
-
 include(Pj/DetectArch)
 pj_detect_arch(arch)
 pj_detect_arch_simd_ext(simd_inst simd_flags)
 
-target_compile_options(${OPTS_TARGET} INTERFACE ${simd_flags})
+set(WEBRTC_ARCH "${arch}")
+set(WEBRTC_ARCH_SIMD "${arch}")
+set(WEBRTC_COMPILE_OPTIONS ${simd_flags})
+set(WEBRTC_COMPILE_DEFINITIONS WEBRTC_APM_DEBUG_DUMP=0 WEBRTC_POSIX=1)
 
-target_compile_definitions(${OPTS_TARGET}
-  INTERFACE
-    WEBRTC_APM_DEBUG_DUMP=0
-    WEBRTC_POSIX=1
+# operating system definitions
+if(IOS)
+  list(APPEND WEBRTC_COMPILE_DEFINITIONS WEBRTC_IOS=1)
+elseif(DARWIN)
+  list(APPEND WEBRTC_COMPILE_DEFINITIONS WEBRTC_MAC=1)
+elseif(ANDROID)
+  list(APPEND WEBRTC_COMPILE_DEFINITIONS WEBRTC_ANDROID=1)
+elseif(LINUX)
+  list(APPEND WEBRTC_COMPILE_DEFINITIONS WEBRTC_LINUX=1)
+elseif(WIN32)
+  list(APPEND WEBRTC_COMPILE_DEFINITIONS WEBRTC_WIN=1)
+endif()
 
-    # os detecton
-    $<$<BOOL:${IOS}>:WEBRTC_IOS=1>
-    $<$<BOOL:${DARWIN}>:WEBRTC_MAC=1>
-    $<$<BOOL:${ANDROID}>:WEBRTC_ANDROID=1>
-    $<$<BOOL:${LINUX}>:WEBRTC_LINUX=1>
-    $<$<BOOL:${WIN32}>:WEBRTC_WIN=1>
+# architecture definitions
+if(arch STREQUAL "arm64")
+  list(APPEND WEBRTC_COMPILE_DEFINITIONS WEBRTC_ARCH_ARM64=1)
+elseif(arch STREQUAL "mips")
+  list(APPEND WEBRTC_COMPILE_DEFINITIONS MIPS_FPU_LE=1)
+endif()
 
-    # architecture
-    $<$<STREQUAL:${arch},arm64>:WEBRTC_ARCH_ARM64=1>
-    $<$<STREQUAL:${arch},mips>:MIPS_FPU_LE=1>
-    $<$<STREQUAL:${simd_inst},neon>:WEBRTC_HAS_NEON=1>
-)
-
-set_target_properties(${OPTS_TARGET} PROPERTIES
-  WEBRTC_ARCH "${arch}"
-  WEBRTC_ARCH_SIMD "${simd_inst}"
-)
+if(simd_inst STREQUAL "neon")
+  list(APPEND WEBRTC_COMPILE_DEFINITIONS WEBRTC_HAS_NEON=1)
+endif()
