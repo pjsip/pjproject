87cf352d6eb53a8b5f12d661b5c1a34b4a7d0e2e
Ayman Al-Qadhi <alqd@protonmail.com>
2025-09-04 03:15:18 +0300

    Remove untested module

--- cmake/SelectRuntime.cmake
+++
@@ -1,7 +0,0 @@
-option(PJ_USE_STATIC_RUNTIME "Use static runtime" OFF)
-
-if (PJ_USE_STATIC_RUNTIME)
-  if (MSVC)
-    set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
-  endif()
-endif()
