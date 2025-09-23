d1c753cec765ef856f9993b0629ce880706e6a38
iarizmendi <iarizmendi@interactions.com>
2025-09-23 09:30:19 -0400

    pjsua2; replace IoQueue with Ioqueue in Endpoint::libGetSipIoqueueFd and Endpoint::libGetMediaIoqueueFd

--- pjsip/src/pjsua2/endpoint.cpp
+++ pjsip/src/pjsua2/endpoint.cpp
@@ -2244,14 +2244,14 @@ void Endpoint::libDestroy(unsigned flags) PJSUA2_THROW(Error)
     PJSUA2_CHECK_RAISE_ERROR(status);
 }
 
-int Endpoint::libGetSipIoQueueFd()
+int Endpoint::libGetSipIoqueueFd()
 {
     pjsip_endpoint* endp = pjsua_get_pjsip_endpt();
     pj_ioqueue_t* ioq = pjsip_endpt_get_ioqueue(endp);
     return pj_ioqueue_get_os_fd(ioq);
 }
 
-int Endpoint::libGetMediaIoQueueFd()
+int Endpoint::libGetMediaIoqueueFd()
 {
     pjmedia_endpt* endp = pjsua_get_pjmedia_endpt();
     pj_ioqueue_t* ioq = pjmedia_endpt_get_ioqueue(endp);
