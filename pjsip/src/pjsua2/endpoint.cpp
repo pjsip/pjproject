ce31d021d56ca2d185c7978e3d552e4d5309aa97
Nanang Izzuddin <nanang@teluu.com>
2025-11-17 09:45:48 +0700

    PJSUA2 call.hpp should include account.hpp
    This clarifies the header dependencies, some types used in `call.hpp` are defined in `account.hpp`.

--- pjsip/src/pjsua2/endpoint.cpp
+++ pjsip/src/pjsua2/endpoint.cpp
@@ -16,7 +16,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
  */
 #include <pjsua2/endpoint.hpp>
-#include <pjsua2/account.hpp>
 #include <pjsua2/call.hpp>
 #include <pjsua2/presence.hpp>
 #include <algorithm>
