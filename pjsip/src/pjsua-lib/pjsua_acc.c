dafe1c4419af3a66afaeb98f06de6d80f96aff13
Riza Sulistyo <trengginas@users.noreply.github.com>
2026-03-23 07:51:56 +0700

    reset rfc5626_status when connection disconnect

--- pjsip/src/pjsua-lib/pjsua_acc.c
+++ pjsip/src/pjsua-lib/pjsua_acc.c
@@ -4586,6 +4586,7 @@ void pjsua_acc_on_tp_state_changed(pjsip_transport *tp,
                 continue;
 
             pjsip_regc_release_transport(pjsua_var.acc[i].regc);
+            acc->rfc5626_status = OUTBOUND_UNKNOWN;
 
             if (pjsua_var.acc[i].ip_change_op ==
                                             PJSUA_IP_CHANGE_OP_ACC_SHUTDOWN_TP)
