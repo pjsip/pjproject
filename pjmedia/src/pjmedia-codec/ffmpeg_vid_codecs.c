pjsip/pjproject

commit 0369f2c2fcb38be64252a56e81d5308e436112c2

Fix compile error with ffmpeg master branch (#4425)

pjmedia/src/pjmedia-codec/ffmpeg_vid_codecs.c
@@ -1967,8 +1967,14 @@ static pj_status_t ffmpeg_codec_decode_whole(pjmedia_vid_codec *codec,
         /* Check decoding result, e.g: see if the format got changed,
          * keyframe found/missing.
          */
+#if LIBAVUTIL_VER_AT_LEAST(58,7)
         status = check_decode_result(codec, &input->timestamp,
-                                     avframe.key_frame);
+                !!(avframe.flags & AV_FRAME_FLAG_KEY));
+#else
+        status = check_decode_result(codec, &input->timestamp,
+                avframe.key_frame);
+#endif
+                                     /* avframe.key_frame); */
         if (status != PJ_SUCCESS) {
             ffmpeg_frame_unref(&avframe);
             return status;

pjmedia/src/pjmedia/ffmpeg_util.h
@@ -49,6 +49,18 @@
                                                (LIBAVCODEC_VERSION_MAJOR == major && \
                                                 LIBAVCODEC_VERSION_MINOR >= minor))
 
+#define LIBAVFORMAT_VER_AT_LEAST(major,minor)  (LIBAVFORMAT_VERSION_MAJOR > major || \
+                                                (LIBAVFORMAT_VERSION_MAJOR == major && \
+                                                 LIBAVFORMAT_VERSION_MINOR >= minor))
+
+#define LIBAVUTIL_VER_AT_LEAST(major,minor)  (LIBAVUTIL_VERSION_MAJOR > major || \
+                                              (LIBAVUTIL_VERSION_MAJOR == major && \
+                                               LIBAVUTIL_VERSION_MINOR >= minor))
+
+#if LIBAVCODEC_VER_AT_LEAST(60,39)
+#   define avcodec_close(x)     ((void)0)
+#endif
+
 void pjmedia_ffmpeg_add_ref();
 void pjmedia_ffmpeg_dec_ref();
 

pjmedia/src/pjmedia-videodev/ffmpeg_dev.c
@@ -411,7 +411,11 @@ static pj_status_t ffmpeg_factory_refresh(pjmedia_vid_dev_factory *f)
         unsigned dev_cnt = MAX_DEV_CNT;
         unsigned dev_idx;
 
+#if LIBAVFORMAT_VER_AT_LEAST(60, 21)
+        if ((p->flags & AVFMT_NOFILE)==0) {
+#else
         if ((p->flags & AVFMT_NOFILE)==0 || p->read_probe) {
+#endif
             goto next_format;
         }
 

commit a85801b6c81c364700a5a56839ad2c4a7bee10d8

Properly close FFmpeg codec context
with libavcodec 60.39 and newer, in order to prevent leaking memory,
threads and other resources. This fixes a regression in commit
0369f2c2fcb38be64252a56e81d5308e436112c2.

pjmedia/src/pjmedia-codec/ffmpeg_vid_codecs.c
@@ -1271,12 +1271,17 @@ static void print_ffmpeg_err(int err)
 
 static void free_codec_context(AVCodecContext **ctx, pj_bool_t opened)
 {
+#if LIBAVCODEC_VER_AT_LEAST(60,39)
+    PJ_UNUSED_ARG(opened);
+    avcodec_free_context(ctx);
+#else
     if (!*ctx)
         return;
     if (opened)
         avcodec_close(*ctx);
     av_free(*ctx);
     *ctx = NULL;
+#endif
 }
 
 static pj_status_t open_ffmpeg_codec(ffmpeg_private *ff,

pjmedia/src/pjmedia/ffmpeg_util.h
@@ -57,10 +57,6 @@
                                               (LIBAVUTIL_VERSION_MAJOR == major && \
                                                LIBAVUTIL_VERSION_MINOR >= minor))
 
-#if LIBAVCODEC_VER_AT_LEAST(60,39)
-#   define avcodec_close(x)     ((void)0)
-#endif
-
 void pjmedia_ffmpeg_add_ref();
 void pjmedia_ffmpeg_dec_ref();
 