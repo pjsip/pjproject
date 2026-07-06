282e8d58677728afcb5e9027ca24cf485f2f9e4b
nanang <nanang@teluu.com>
2026-07-06 15:05:17 +0700

    add contributor credit, clean up license header

--- pjmedia/include/pjmedia/tone_detector.h
+++ pjmedia/include/pjmedia/tone_detector.h
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2008-2011 Teluu Inc. (http://www.teluu.com)
- * Copyright (C) 2003-2008 Benny Prijono <benny@prijono.org>
+ * Copyright (C) 2026 Teluu Inc. (http://www.teluu.com)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,6 +14,11 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
+ * Contributed by:
+ *  Julien Chavanton <jchavanton@gmail.com>
  */
 #ifndef __PJMEDIA_TONE_DETECTOR_H__
 #define __PJMEDIA_TONE_DETECTOR_H__
